bce738c77d9b834071c4ccc57946ece7e4b05728
Norman Koch <s3811141@login1.barnard.hpc.tu-dresden.de>
2023-12-04 14:34:24 +0100

    prototype for barnard, doesnt work yet

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LMOD_DIR=/software/foundation/x86_64/lmod/lmod/libexec
+
+ml () { 
+    eval "$($LMOD_DIR/ml_cmd "$@")"
+}
+
+ml  release/23.10 GCCcore/11.3.0 Perl/5.34.1  GCC/11.3.0  OpenMPI/4.1.4 numpy/1.21.6-Python-3.10.4
+
+perl sbatch.pl $*

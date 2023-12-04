68da11a5f647d8e3d9d0acb48af4df2b0b3d8ba1
Norman Koch <s3811141@n1630.barnard.hpc.tu-dresden.de>
2023-12-04 15:20:17 +0100

    other mongodb version?

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ module () {
         eval `$LMOD_CMD sh "$@"`
 }
 
-ml release/23.10 GCCcore/11.3.0 Perl/5.34.1  GCC/11.3.0  OpenMPI/4.1.4 numpy/1.21.6-Python-3.10.4
+ml release/23.10 GCCcore/11.3.0 Perl/5.34.1  GCC/11.3.0  OpenMPI/4.1.4 numpy/1.21.6-Python-3.10.4 MongoDB/6.0.4
 
 pip3 install hyperopt
 
--- sbatch.pl
+++ sbatch.pl
@@ -877,7 +877,6 @@ sub load_needed_modules {
                 );
         } else {
                 push @modules, (
-                        'modenv/scs5',
                         'MongoDB/4.0.3',
                         'Hyperopt/0.2.2-fosscuda-2019b-Python-3.7.4',
                         'Python/3.7.4-GCCcore-8.3.0',

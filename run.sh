db5373085941f6e1a4f36fb74b418b035b15d39c
Norman Koch <s3811141@login1.barnard.hpc.tu-dresden.de>
2023-12-04 14:50:23 +0100

    added hyperopt

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ ml () {
     eval "$($LMOD_DIR/ml_cmd "$@")"
 }
 
-ml  release/23.10 GCCcore/11.3.0 Perl/5.34.1  GCC/11.3.0  OpenMPI/4.1.4 numpy/1.21.6-Python-3.10.4
+ml release/23.10 GCCcore/11.3.0 Perl/5.34.1  GCC/11.3.0  OpenMPI/4.1.4 numpy/1.21.6-Python-3.10.4
+
+pip3 install hyperopt
 
 perl sbatch.pl $*

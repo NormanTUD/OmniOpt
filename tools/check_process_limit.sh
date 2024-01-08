7b037a95a0434369dc945c77af3d1265f9d2a678
norman koch alpha <norman.koch@tu-dresden.de>
2024-01-08 15:21:00 +0100

    fix

--- tools/check_process_limit.sh
+++ tools/check_process_limit.sh
@@ -3,12 +3,11 @@
 LOGFILENAME=$1
 
 CURRENTNUMPROCESSES=$(ps h -Lu $USER | wc -l)
-MAXNUMPROCESSES=$(cat /etc/security/limits.d/20-nproc.conf | grep "^\*" | sed -e 's/.*nproc\s*//')
 HUMANDATE=$(date '+%Y.%m.%d %H:%M:%S')
 UNIXTIMESTAMP=$(date +%s)
 
 if [[ ! -s $LOGFILENAME ]]; then
-    echo "time,timestamp,currentnumprocesses,maxnumprocesses"
+    echo "time,timestamp,currentnumprocesses"
 fi
 
-echo "$HUMANDATE,$UNIXTIMESTAMP,$CURRENTNUMPROCESSES,$MAXNUMPROCESSES"
+echo "$HUMANDATE,$UNIXTIMESTAMP,$CURRENTNUMPROCESSES"

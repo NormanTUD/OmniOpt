f22f3a572a193b6e10ebc809a74ba3b872711ec9
norman koch alpha <norman.koch@tu-dresden.de>
2024-01-10 12:05:29 +0100

    check for existance of projects

--- evaluate-run.sh
+++ evaluate-run.sh
@@ -1053,7 +1053,7 @@ button=black,white
 function plot_multiple_projects {
     PROJECTS=$(ls $PROJECTDIR/*/config.ini | sed -e "s#$PROJECTDIR/##" | sed -e 's#/config.ini##' 2>/dev/null)
 
-    if [[ -z "$PROJECTS" ]];
+    if [[ -z "$PROJECTS" ]]; then
 	error_message "No projects found"
 	return
     fi
@@ -1504,7 +1504,11 @@ function change_variables {
 }
 
 function list_projects {
-	AVAILABLE_PROJECTS=$(ls $PROJECTDIR/*/config.ini | sed -e "s#${PROJECTDIR}/##" | sed -e 's#/config.ini##' | perl -le 'while (<>) { chomp; chomp; print qq#$_ $_# }')
+	AVAILABLE_PROJECTS=$(ls $PROJECTDIR/*/config.ini | sed -e "s#${PROJECTDIR}/##" | sed -e 's#/config.ini##' | perl -le 'while (<>) { chomp; chomp; print qq#$_ $_# }' 2>/dev/null)
+
+	if [[ -z "$AVAILABLE_PROJECTS" ]]; then
+		echo "No projects found"
+	fi
 
 	eval `resize`
 

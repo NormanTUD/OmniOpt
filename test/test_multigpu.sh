65f627147506c31688780e838d2fad3121004976
norman koch alpha <norman.koch@tu-dresden.de>
2024-01-11 13:52:59 +0100

    fix

--- test/test_multigpu.sh
+++ test/test_multigpu.sh
@@ -15,7 +15,9 @@ JOBNAME=multigpu_test_${NUMGPUS}
 
 RESULT=$(bash tools/multigpu.sh --jobname=$JOBNAME --partition=alpha --num_gpus=$NUMGPUS --force_redo --maxtime=00:05:00 --programfile=test/list_num_of_gpus.sh | grep Num | sed -e 's/.*: //')
 
-echo $RESULT
+echo "RESULT >>>"
+echo "$RESULT"
+echo "<<<<<<<<<<"
 
 if [[ "$RESULT" -eq "$NUMGPUS" ]]; then
         exit 0

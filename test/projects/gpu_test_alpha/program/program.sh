ada0099c37a8f8e5f89ea5e953863f8fa519988f
norman koch alpha <norman.koch@tu-dresden.de>
2024-01-18 13:40:14 +0100

    repair

--- test/projects/gpu_test_alpha/program/program.sh
+++ test/projects/gpu_test_alpha/program/program.sh
@@ -2,6 +2,11 @@
 
 PARAM=$1
 
+if [[ -z $PARAM ]]; then
+	echo "Missing parameter"
+	exit 2
+fi
+
 LMOD_DIR=/usr/share/lmod/lmod/libexec/
 LMOD_CMD=/usr/share/lmod/lmod/libexec/lmod
 module () {
@@ -19,8 +24,8 @@ ml TensorFlow/2.11.0-CUDA-11.7.0
 
 PYTHONOUTPUT=$(python3 -c "import tensorflow as tf; print(tf.test.gpu_device_name())" 2>&1)
 echo $PYTHONOUTPUT
-PCI_ID=$(echo $PYTHONOUTPUT | grep pciBusID | sed -e "s/^//")
-if echo $PCI_ID | egrep "pciBusID: ([0-9]*:?)+.[0-9]*" >/dev/null 2>/dev/null; then
+PCI_ID=$(echo $PYTHONOUTPUT | grep 'pci bus id' | sed -e "s/^//")
+if echo $PCI_ID | egrep "pci bus id: ([0-9]*:?)+.[0-9]*" >/dev/null 2>/dev/null; then
         python3 -c "print('RESULT: $PARAM')"
 else
         echo "NO GPU FOUND"

a86de0a0fe42cc106324a52ce4de9b8f27909f83
norman koch alpha <norman.koch@tu-dresden.de>
2024-01-22 08:21:09 +0100

    fix

--- test/multiparameter_test.sh
+++ test/multiparameter_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT=$(bash tools/multigpu.sh --partition=ml --num_gpus=0 --force_redo --maxtime=00:05:00 --programfile=test/multiparameter_test_script.sh --jobname="multiparamtestjob" | grep "RESULT:" | sed -e 's/RESULT: //')
+RESULT=$(bash tools/multigpu.sh --num_gpus=0 --force_redo --maxtime=00:05:00 --programfile=test/multiparameter_test_script.sh --jobname="multiparamtestjob" | grep "RESULT:" | sed -e 's/RESULT: //')
 
 if [[ "$RESULT" == "1.77482393492988" ]]; then
     exit 0

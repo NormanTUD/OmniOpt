5722b60919b5a92dd7cbe1de7c267d4f283594bf
Norman Koch <norman.koch@tu-dresden.de>
2024-01-30 11:05:06 +0100

    Added missing space in error message

--- ax/main.py
+++ ax/main.py
@@ -4,6 +4,9 @@ try:
     from rich.console import Console
     console = Console()
     with console.status("[bold green]Importing modules...") as status:
+        from rich.traceback import install
+        install(show_locals=True)
+
         from rich.table import Table
         from rich import print
         from rich.progress import Progress

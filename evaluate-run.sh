52d59bd84b423dc0a94a1d76a6cd7e694c693702
norman koch alpha <norman.koch@tu-dresden.de>
2024-01-31 13:21:49 +0100

    fixed some things

--- evaluate-run.sh
+++ evaluate-run.sh
@@ -124,77 +124,77 @@ for i in "$@"; do
 done
 
 function gaugecommand () {
-    set +x
-    PCT=0
-    TITLE=$1
-    TEXT=$2
-    COMMAND=$3
+	set +x
+	PCT=0
+	TITLE=$1
+	TEXT=$2
+	COMMAND=$3
 
-    COMMANDFILE="$RANDOM.command"
-    while [[ -e $COMMANDFILE ]]; do
-            COMMANDFILE="$RANDOM.command"
-    done
+	COMMANDFILE="$RANDOM.command"
+	while [[ -e $COMMANDFILE ]]; do
+		COMMANDFILE="$RANDOM.command"
+	done
 
-    echo $COMMAND > $COMMANDFILE 2>/dev/null
+	echo $COMMAND > $COMMANDFILE 2>/dev/null
 
-    if [[ "$DISPLAYGAUGE" -eq "1" ]]; then
-            GAUGEFILE=$RANDOM.gauge
-            while [[ -e $GAUGE ]]; do
-                    GAUGEFILE=$RANDOM.gauge
-            done
+	if [[ "$DISPLAYGAUGE" -eq "1" ]]; then
+		GAUGEFILE=$RANDOM.gauge
+		while [[ -e $GAUGE ]]; do
+			GAUGEFILE=$RANDOM.gauge
+		done
 
-            eval `resize`
+		eval `resize`
 
-            (stdbuf -oL -eL bash $COMMANDFILE 2> $GAUGEFILE >/dev/null) &
-            OLDTERM=$TERM
-            export TERM=linux
+		(stdbuf -oL -eL bash $COMMANDFILE 2> $GAUGEFILE >/dev/null) &
+		OLDTERM=$TERM
+		export TERM=linux
 
-            JOBPID=$(jobs -p | sed -e 's/ running.*//' | sed -e 's/.*\+ //')
-            ( 
-                    while ps -auxf | grep -v grep | grep $JOBPID >/dev/null; do
-                            if [[ -e $GAUGEFILE ]]; then
-                                echo "XXX"
-                                CURRENT_PERCENTAGE=$(cat $GAUGEFILE | grep PERCENTGAUGE | sed -e 's/PERCENTGAUGE: //' | tail -n1)
-                                echo $CURRENT_PERCENTAGE
-                                HASMSG=$(cat $GAUGEFILE | grep GAUGESTATUS | tail -n1 | sed -e 's/GAUGESTATUS: //')
-                                if [[ -z $HASMSG ]]; then
-                                    echo "$TEXT"
-                                else
-                                    echo "$TEXT ($HASMSG)"
-                                fi
-                                echo "XXX"
-                                if [[ "$CURRENT_PERCENTAGE" -eq "100" ]]; then
-                                    exit
-                                fi
-                            fi
-                            sleep 1
-                    done
-            ) | whiptail --title "$TITLE" --gauge "$TEXT"  $LINES $COLUMNS $(( $LINES - 8 ))
-            cat $GAUGEFILE
-            rm $GAUGEFILE
-            export TERM=$OLDTERM
-    else
-            bash $COMMANDFILE
-    fi
+		JOBPID=$(jobs -p | sed -e 's/ running.*//' | sed -e 's/.*\+ //')
+		( 
+		while ps -auxf | grep -v grep | grep $JOBPID >/dev/null; do
+			if [[ -e $GAUGEFILE ]]; then
+				echo "XXX"
+				CURRENT_PERCENTAGE=$(cat $GAUGEFILE | grep PERCENTGAUGE | sed -e 's/PERCENTGAUGE: //' | tail -n1)
+				echo $CURRENT_PERCENTAGE
+				HASMSG=$(cat $GAUGEFILE | grep GAUGESTATUS | tail -n1 | sed -e 's/GAUGESTATUS: //')
+				if [[ -z $HASMSG ]]; then
+					echo "$TEXT"
+				else
+					echo "$TEXT ($HASMSG)"
+				fi
+				echo "XXX"
+				if [[ "$CURRENT_PERCENTAGE" -eq "100" ]]; then
+					exit
+				fi
+			fi
+			sleep 1
+		done
+		) | whiptail --title "$TITLE" --gauge "$TEXT"  $LINES $COLUMNS $(( $LINES - 8 ))
+		cat $GAUGEFILE
+		rm $GAUGEFILE
+		export TERM=$OLDTERM
+	else
+		bash $COMMANDFILE
+	fi
 
-    if [[ "$DEBUG" -eq "1" ]]; then
-        set -x
-    fi
-    rm $COMMANDFILE
+	if [[ "$DEBUG" -eq "1" ]]; then
+		set -x
+	fi
+	rm $COMMANDFILE
 }
 
 function info_message_large {
 	MSG=$1
-    eval `resize`
+	eval `resize`
 	echo_green "$MSG"
 	whiptail --title "Info Message" --msgbox "$MSG" $LINES $COLUMNS
 }
 
 function info_message {
-    eval `resize`
+	eval `resize`
 	MSG=$1
 	echo_green "$MSG"
-    whiptail --title "Info Message" --msgbox "$MSG" $LINES $COLUMNS $(( $LINES - 8 ))
+	whiptail --title "Info Message" --msgbox "$MSG" $LINES $COLUMNS $(( $LINES - 8 ))
 }
 
 
@@ -203,8 +203,8 @@ function inputbox {
 	MSG=$2
 	DEFAULT=$3
 
-    eval `resize`
-    RESULT=$(whiptail --inputbox "$MSG" $LINES $COLUMNS "$DEFAULT" --title "$TITLE" 3>&1 1>&2 2>&3)
+	eval `resize`
+	RESULT=$(whiptail --inputbox "$MSG" $LINES $COLUMNS "$DEFAULT" --title "$TITLE" 3>&1 1>&2 2>&3)
 	exitstatus=$?
 	if [[ $exitstatus == 0 ]]; then
 		echo "$RESULT"
@@ -215,7 +215,11 @@ function inputbox {
 }
 
 function change_project_dir {
-    PROJECTDIR=$(inputbox "Projectdir" "The directory '$PROJECTDIR' could not be found. Enter a new Projectdir (relative or absolute)" "$PROJECTDIR")
+	if [[ -d "$PROJECTDIR" ]]; then
+		PROJECTDIR=$(inputbox "Projectdir" "The project directory is currently '$PROJECTDIR'. Enter a new Projectdir (relative or absolute) or just press enter to continue using this one" "$PROJECTDIR")
+	else
+		PROJECTDIR=$(inputbox "Projectdir" "The directory '$PROJECTDIR' could not be found. Enter a new Projectdir (relative or absolute)" "$PROJECTDIR")
+	fi
 }
 
 function get_possible_options_for_gpu_plot {

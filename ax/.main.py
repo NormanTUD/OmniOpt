0c6b9e51966c3b14034d1f98b4521a0eb0d0c919
Norman Koch <norman.koch@tu-dresden.de>
2024-02-01 12:17:32 +0100

    fix

--- ax/.main.py
+++ ax/.main.py
@@ -132,24 +132,24 @@ def parse_experiment_parameters(args):
             valid_types_string = ', '.join(valid_types)
 
             if param_type not in valid_types:
-                print_color("red", f":warning: Invalid type {param_type}, valid types are: {valid_types_string}")
+                print_color("red", f"\n:warning: Invalid type {param_type}, valid types are: {valid_types_string}")
                 sys.exit(3)
 
             if param_type == "range":
                 if len(this_args) != 5 and len(this_args) != 4:
-                    print_color("red", f":warning: --parameter for type range must have 5 parameters: <NAME> range <START> <END> (<TYPE (int or float)>)");
+                    print_color("red", f"\n:warning: --parameter for type range must have 5 parameters: <NAME> range <START> <END> (<TYPE (int or float)>)");
                     sys.exit(9)
 
                 try:
                     lower_bound = float(this_args[j + 2])
                 except:
-                    print_color("red", f":warning: {this_args[j + 2]} does not seem to be a number")
+                    print_color("red", f"\n:warning: {this_args[j + 2]} does not seem to be a number")
                     sys.exit(4)
 
                 try:
                     upper_bound = float(this_args[j + 3])
                 except:
-                    print_color("red", f":warning: {this_args[j + 3]} does not seem to be a number")
+                    print_color("red", f"\n:warning: {this_args[j + 3]} does not seem to be a number")
                     sys.exit(5)
 
                 if upper_bound == lower_bound:
@@ -175,7 +175,7 @@ def parse_experiment_parameters(args):
 
                 if value_type not in valid_value_types:
                     ", ".join(valid_value_types)
-                    print_color("red", f":warning: {value_type} is not a valid value type. Valid types for range are: {valid_value_types_string}")
+                    print_color("red", f"\n:warning: {value_type} is not a valid value type. Valid types for range are: {valid_value_types_string}")
                     sys.exit(8)
 
                 param = {
@@ -190,13 +190,13 @@ def parse_experiment_parameters(args):
                 j += skip
             elif param_type == "fixed":
                 if len(this_args) != 3:
-                    print_color("red", f":warning: --parameter for type fixed must have 3 parameters: <NAME> range <VALUE>");
+                    print_color("red", f"\n:warning: --parameter for type fixed must have 3 parameters: <NAME> range <VALUE>");
                     sys.exit(11)
 
                 try:
                     value = float(this_args[j + 2])
                 except:
-                    print_color("red", f":warning: {this_args[j + 2]} does not seem to be a number")
+                    print_color("red", f"\n:warning: {this_args[j + 2]} does not seem to be a number")
                     sys.exit(12)
 
                 param = {
@@ -210,7 +210,7 @@ def parse_experiment_parameters(args):
                 j += 3
             elif param_type == "choice":
                 if len(this_args) != 3:
-                    print_color("red", f":warning: --parameter for type choice must have 3 parameters: <NAME> choice <VALUE,VALUE,VALUE>");
+                    print_color("red", f"\n:warning: --parameter for type choice must have 3 parameters: <NAME> choice <VALUE,VALUE,VALUE>");
                     sys.exit(11)
 
                 values = re.split(r'\s*,\s*', str(this_args[j + 2]))
@@ -228,7 +228,7 @@ def parse_experiment_parameters(args):
 
                 j += 3
             else:
-                print_color("red", f":warning: Parameter type {param_type} not yet implemented.");
+                print_color("red", f"\n:warning: Parameter type {param_type} not yet implemented.");
                 sys.exit(14)
         i += 1
 
@@ -277,7 +277,7 @@ def replace_parameters_in_string(parameters, input_string):
 
         return input_string
     except Exception as e:
-        print_color("red", f":warning: Error: {e}")
+        print_color("red", f"\n:warning: Error: {e}")
         return None
 
 def execute_bash_code(code):
@@ -392,14 +392,14 @@ try:
             from ax.service.ax_client import AxClient, ObjectiveProperties
             from ax.service.utils.report_utils import exp_to_df
         except:
-            print_color("red", ":warning: ax could not be loaded. Did you create and load the virtual environment properly?")
+            print_color("red", "\n:warning: ax could not be loaded. Did you create and load the virtual environment properly?")
             sys.exit(6)
 
         try:
             import submitit
             from submitit import AutoExecutor, LocalJob, DebugJob
         except:
-            print_color("red", ":warning: submitit could not be loaded. Did you create and load the virtual environment properly?")
+            print_color("red", "\n:warning: submitit could not be loaded. Did you create and load the virtual environment properly?")
             sys.exit(7)
 except KeyboardInterrupt:
     sys.exit(0)
@@ -495,9 +495,9 @@ try:
                         progress.update(progress_bar, advance=1)
                     except ax.exceptions.core.UserInputError as error:
                         if "None for metric" in str(error):
-                            print_color("red", f":warning: It seems like the program that was about to be run didn't have 'RESULT: <NUMBER>' in it's output string.\nError: {error}")
+                            print_color("red", f"\n:warning: It seems like the program that was about to be run didn't have 'RESULT: <NUMBER>' in it's output string.\nError: {error}")
                         else:
-                            print_color("red", ":warning: ".error)
+                            print_color("red", f"\n:warning: {error}")
                             sys.exit(1)
             
             # Schedule new jobs if there is availablity
@@ -513,9 +513,9 @@ try:
                     time.sleep(1)
                 except submitit.core.utils.FailedJobError as error:
                     if "QOSMinGRES" in str(error) and args.gpus == 0:
-                        print_color("red", f":warning: It seems like, on the chosen partition, you need at least one GPU. Use --gpus=1 (or more) as parameter.")
+                        print_color("red", f"\n:warning: It seems like, on the chosen partition, you need at least one GPU. Use --gpus=1 (or more) as parameter.")
                     else:
-                        print_color("red", f":warning: FAILED: {error}")
+                        print_color("red", f"\n:warning: FAILED: {error}")
 
                     sys.exit(2)
             
@@ -523,7 +523,7 @@ try:
             # If you have a large number of jobs, consider adding a sleep statement in the job polling loop aswell.
             time.sleep(0.1)
 except KeyboardInterrupt:
-    print_color("red", ":warning: You pressed CTRL+C. Program execution halted.")
+    print_color("red", "\n:warning: You pressed CTRL+C. Program execution halted.")
 
 try:
     best_parameters, (means, covariances) = ax_client.get_best_parameters()
@@ -549,9 +549,9 @@ try:
     # Drucke die Tabelle
     console.print(table)
 except KeyboardInterrupt:
-    print_color("red", ":warning: You pressed CTRL+C. Program execution halted.")
+    print_color("red", "\n:warning: You pressed CTRL+C. Program execution halted.")
 except TypeError:
-    print_color("red", ":warning: The program has been halted without attaining any tangible results.")
+    print_color("red", "\n:warning: The program has been halted without attaining any tangible results.")
 
 pd_csv = f'runs/{args.experiment_name}/{folder_number}/pd.csv'
 print_color("green", f"Saving result pandas data frame to {pd_csv}")

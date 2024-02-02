f315c63245c8e08f2a576dea51858812c2aa50ea
Norman Koch <norman.koch@tu-dresden.de>
2024-02-02 19:35:49 +0100

    fix

--- ax/.plot.py
+++ ax/.plot.py
@@ -2,6 +2,26 @@ import sys
 import os
 import argparse
 
+def check_environment_variable(variable_name):
+    try:
+        value = os.environ[variable_name]
+        return True
+    except KeyError:
+        return False
+
+if not check_environment_variable("RUN_VIA_RUNSH"):
+    print("Must be run via run.sh, cannot be run as standalone.")
+
+    sys.exit(16)
+
+def in_venv():
+    return sys.prefix != sys.base_prefix
+
+
+if not in_venv():
+    print("No venv loaded. Cannot continue.")
+    sys.exit(19)
+
 try:
     import pandas as pd
     import matplotlib.pyplot as plt

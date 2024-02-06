9bd6046ce408b1245d66a6e5f021ba63f0c99dc0
Norman Koch <norman.koch@tu-dresden.de>
2024-02-06 14:04:54 +0100

    fix

--- ax/.tools/create_parallel_plot.py
+++ ax/.tools/create_parallel_plot.py
@@ -9,8 +9,8 @@ output_file = sys.argv[2]
 csv_file_stripped = csv_file + "_stripped.csv"
 
 enabled_titles = os.getenv("enabled_titles", "")
-keep_cols = enabled_titles.split(";")
-cols = list(pd.read_csv(csv_file, nrows =1, sep=';'))
+keep_cols = enabled_titles.split(",")
+cols = list(pd.read_csv(csv_file, nrows =1, sep=','))
 f = pd.read_csv(csv_file, sep=",", usecols = [i for i in cols if i in keep_cols])
 f.to_csv(csv_file_stripped, index=False)
 

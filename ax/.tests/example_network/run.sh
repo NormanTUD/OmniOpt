2f1353ff4a10ea2b815048edea9c9fbf8e14e186
norman koch alpha <norman.koch@tu-dresden.de>
2024-03-12 10:31:31 +0100

    fix

--- ax/.tests/example_network/run.sh
+++ ax/.tests/example_network/run.sh
@@ -63,6 +63,16 @@ for i in $@; do
         esac
 done
 
+LMOD_DIR=/software/foundation/$(uname -m)/lmod/lmod/libexec
+
+if [[ -d $LMOD_DIR ]]; then
+	ml () {
+		eval "$($LMOD_DIR/ml_cmd "$@")"
+	}
+
+	ml release/23.10 GCC/11.3.0 OpenMPI/4.1.4 TensorFlow/2.11.0-CUDA-11.7.0
+fi
+
 ENV_DIR=$HOME/.test_env
 if [[ ! -d "$ENV_DIR" ]]; then
         green_text "$ENV_DIR not found. Creating virtual environment."

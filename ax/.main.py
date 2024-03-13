4fe52ea7f83b5782bd5ed84ff1230f5766b92951
norman koch alpha <norman.koch@tu-dresden.de>
2024-03-13 09:33:32 +0100

    fix

--- ax/.main.py
+++ ax/.main.py
@@ -1911,6 +1911,11 @@ def log_nr_of_workers ():
     last_line = ""
     nr_of_workers = get_number_of_current_workers()
 
+    with open(logfile_nr_workers) as f:
+        for line in f:
+            pass
+        last_line = line.strip()
+
     if (last_line.isnumeric() or last_line == "") and str(last_line) != str(nr_of_workers):
         with open(logfile_nr_workers, 'a+') as f:
             f.write(str(nr_of_workers) + "\n")

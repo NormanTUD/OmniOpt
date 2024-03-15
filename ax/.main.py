245821294a5df47aadd499fa05d9c1c42292eb27
Norman Koch <norman.koch@tu-dresden.de>
2024-03-15 17:10:11 +0100

    fix

--- ax/.main.py
+++ ax/.main.py
@@ -1880,8 +1880,6 @@ def analyze_out_files (rootdir):
 def is_tool(name):
     """Check whether `name` is on PATH and marked as executable."""
 
-    # from whichcraft import which
-
     return which(name) is not None
 
 def get_current_workers ():

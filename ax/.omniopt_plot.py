4cb2bc69a009e6d0c582c39ad3ae10b7450239c3
Norman Koch <norman.koch@tu-dresden.de>
2024-05-03 14:02:18 +0200

    fix

--- ax/.omniopt_plot.py
+++ ax/.omniopt_plot.py
@@ -201,7 +201,7 @@ def hide_empty_plots(parameter_combinations, num_rows, num_cols, axs):
         col = i % num_cols
         axs[row, col].set_visible(False)
 
-def plot_multiple_graphs(args, fig, non_empty_graphs, num_cols, axs, df_filtered, colors, cmap, norm, BUBBLESIZEINPX, result_column, parameter_combinations, num_rows):
+def plot_multiple_graphs(args, fig, non_empty_graphs, num_cols, axs, df_filtered, colors, cmap, norm, result_column, parameter_combinations, num_rows):
     for i, (param1, param2) in enumerate(non_empty_graphs):
         row = i // num_cols
         col = i % num_cols
@@ -235,7 +235,7 @@ def plot_multiple_graphs(args, fig, non_empty_graphs, num_cols, axs, df_filtered
     cbar.formatter.set_scientific(False)
     cbar.formatter.set_useMathText(False)
 
-def plot_two_graphs(args, axs, df_filtered, non_empty_graphs, colors, cmap, norm, BUBBLESIZEINPX, result_column):
+def plot_two_graphs(args, axs, df_filtered, non_empty_graphs, colors, cmap, norm, result_column):
     scatter = axs.scatter(df_filtered[non_empty_graphs[0][0]], df_filtered[non_empty_graphs[0][1]], c=colors, cmap=cmap, norm=norm, s=BUBBLESIZEINPX)
     axs.set_xlabel(non_empty_graphs[0][0])
     axs.set_ylabel(non_empty_graphs[0][1])
@@ -251,7 +251,7 @@ def plot_two_graphs(args, axs, df_filtered, non_empty_graphs, colors, cmap, norm
             print(f"File {args.save_to_file} already exists. It will be overwritten.")
         fig.savefig(f"single_plot_{0}_{1}_args.save_to_file")
 
-def plot_single_graph (args, fig, axs, df_filtered, colors, cmap, norm, BUBBLESIZEINPX, result_column, non_empty_graphs):
+def plot_single_graph (args, fig, axs, df_filtered, colors, cmap, norm, result_column, non_empty_graphs):
     ax = axs  # Use the single axis
     _range = range(len(df_filtered))
     _data = df_filtered
@@ -280,7 +280,7 @@ def plot_single_graph (args, fig, axs, df_filtered, colors, cmap, norm, BUBBLESI
             print(f"File {args.save_to_file} already exists. It will be overwritten.")
         fig.savefig(f"single_plot_{0}_args.save_to_file")
 
-def plot_graphs(df, args, fig, axs, df_filtered, BUBBLESIZEINPX, result_column, non_empty_graphs, num_subplots, parameter_combinations, num_rows, num_cols):
+def plot_graphs(df, args, fig, axs, df_filtered, result_column, non_empty_graphs, num_subplots, parameter_combinations, num_rows, num_cols):
     colors = get_colors(df, result_column)
 
     if os.path.exists(args.run_dir + "/maximize"):
@@ -301,11 +301,11 @@ def plot_graphs(df, args, fig, axs, df_filtered, BUBBLESIZEINPX, result_column, 
 
     if num_subplots == 1:
         if len(non_empty_graphs[0]) == 1:
-            plot_single_graph(args, fig, axs, df_filtered, colors, cmap, norm, BUBBLESIZEINPX, result_column, non_empty_graphs)
+            plot_single_graph(args, fig, axs, df_filtered, colors, cmap, norm, result_column, non_empty_graphs)
         else:
-            plot_two_graphs(args, axs, df_filtered, non_empty_graphs, colors, cmap, norm, BUBBLESIZEINPX, result_column)
+            plot_two_graphs(args, axs, df_filtered, non_empty_graphs, colors, cmap, norm, result_column)
     else:
-        plot_multiple_graphs(args, fig, non_empty_graphs, num_cols, axs, df_filtered, colors, cmap, norm, BUBBLESIZEINPX, result_column, parameter_combinations, num_rows)
+        plot_multiple_graphs(args, fig, non_empty_graphs, num_cols, axs, df_filtered, colors, cmap, norm, result_column, parameter_combinations, num_rows)
 
     hide_empty_plots(parameter_combinations, num_rows, num_cols, axs)
 
@@ -444,7 +444,7 @@ def main(args):
 
     fig, axs = plt.subplots(num_rows, num_cols, figsize=(15*num_cols, 7*num_rows))
 
-    plot_graphs(df, args, fig, axs, df_filtered, BUBBLESIZEINPX, result_column, non_empty_graphs, num_subplots, parameter_combinations, num_rows, num_cols)
+    plot_graphs(df, args, fig, axs, df_filtered, result_column, non_empty_graphs, num_subplots, parameter_combinations, num_rows, num_cols)
 
     result_column_values = get_result_column_values(df, result_column)
 

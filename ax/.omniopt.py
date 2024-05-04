7357677c5daaed1e03d0b417dded24647b9f8ecc
Norman Koch <norman.koch@tu-dresden.de>
2024-05-04 23:45:15 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -1708,8 +1708,6 @@ def main ():
                 GenerationStep(
                     model=Models.SOBOL,
                     num_trials=sobol_steps,
-                    #num_trials=args.num_parallel_jobs,  # How many trials should be produced from this generation step
-                    #min_trials_observed=args.num_parallel_jobs,  # How many trials need to be completed to move to next model
                     max_parallelism=args.num_parallel_jobs,  # Max parallelism for this step
                     model_kwargs={"seed": args.seed},  # Any kwargs you want passed into the model
                     model_gen_kwargs={'enforce_num_arms': True},  # Any kwargs you want passed to `modelbridge.gen`
@@ -1718,9 +1716,8 @@ def main ():
                 # from all data available at the time of each new candidate generation call)
                 GenerationStep(
                     model=Models.BOTORCH_MODULAR,
-                    num_trials=-1,  # No limitation on how many trials should be produced from this step
-                    #min_trials_observed=1,  # How many trials need to be completed to move to next model
-                    max_parallelism=args.max_eval,  # Parallelism limit for this step, often lower than for Sobol
+                    num_trials=args.max_eval,  # No limitation on how many trials should be produced from this step
+                    max_parallelism=args.num_parallel_jobs,  # Parallelism limit for this step, often lower than for Sobol
                     #model_kwargs={"seed": args.seed},  # Any kwargs you want passed into the model
                     model_gen_kwargs={'enforce_num_arms': True},  # Any kwargs you want passed to `modelbridge.gen`
                     # More on parallelism vs. required samples in BayesOpt:

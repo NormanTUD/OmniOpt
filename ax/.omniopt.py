772766e4d78c76bf95cd526c492f04dd3a815f40
Norman Koch <norman.koch@tu-dresden.de>
2024-05-08 08:47:44 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -2022,8 +2022,6 @@ def create_and_execute_next_runs (args, ax_client, next_nr_steps, executor):
     try:
         print_debug("Trying to get trial_index_to_param")
 
-        trial_counter = 0
-
         try:
             for i in range(1, next_nr_steps + 1):
                 progressbar_description([f"trying to get parameter set [{i}/{next_nr_steps}]"])
@@ -2032,7 +2030,7 @@ def create_and_execute_next_runs (args, ax_client, next_nr_steps, executor):
                 progressbar_description([f"got parameter set [{i}/{next_nr_steps}]"])
 
                 for trial_index, parameters in trial_index_to_param.items():
-                    trial_counter = execute_evaluation(args, trial_index_to_param, ax_client, trial_index, parameters, trial_counter, executor, next_nr_steps)
+                    execute_evaluation(args, trial_index_to_param, ax_client, trial_index, parameters, i, executor, next_nr_steps)
         except botorch.exceptions.errors.InputDataError as e:
             print_color("red", f"Error 1: {e}")
             return 0

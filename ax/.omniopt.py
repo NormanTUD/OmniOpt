4cf170af33f06b1e25c77b7b94d4eefbe10903c9
Norman Koch <norman.koch@tu-dresden.de>
2024-05-24 16:28:56 +0200

    fixed getting experiments from multiple paths

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -2018,7 +2018,7 @@ def load_data_from_existing_run_folders(args, _paths):
 
                     ax_client.complete_trial(trial_index=new_old_trial[1], raw_data=old_result)
                 else:
-                    print_color("red", "old result for {old_arm_parameter} could not be found in {this_path_json}. If it exists in other files, it will probably be added.")
+                    print_color("red", f"old result for {old_arm_parameter} could not be found in {this_path_json}. If it exists in other files, it will probably be added.")
 
         else:
             print_color("red", f"{this_path_json} does not exist, cannot load data from it")

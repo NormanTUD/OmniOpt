428d7981a60f1b19bbab2cded2e507d50f20eb3f
Norman Koch <norman.koch@tu-dresden.de>
2024-05-26 10:56:25 +0200

    properly check for existance of files in general plot

--- ax/.omniopt_plot_worker.py
+++ ax/.omniopt_plot_worker.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import argparse
 import os
 import pandas as pd
@@ -9,17 +10,29 @@ def plot_worker_usage(pd_csv):
 
         data['time'] = data['time'].apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
 
-        plt.plot(data['time'], data['num_parallel_jobs'], label='Number of Parallel Jobs')
+        plt.plot(data['time'], data['num_parallel_jobs'], label='Requested Number of Workers')
         plt.plot(data['time'], data['nr_current_workers'], label='Number of Current Workers')
         plt.xlabel('Time')
         plt.ylabel('Count')
         plt.title('Worker Usage Plot')
         plt.legend()
+
+        # Reduziere die Anzahl der x-Achsenbeschriftungen
+        num_ticks = 10
+        x_ticks_indices = range(0, len(data['time']), len(data['time']) // num_ticks)
+        x_tick_labels = [data['time'][i] for i in x_ticks_indices]
+        plt.xticks(x_ticks_indices, x_tick_labels, rotation=45)
+
+        plt.ylim(bottom=0.238)  # Setze bottom auf 0.238
+
+        plt.tight_layout()  # Optimiere Layout, um Überlappungen zu minimieren
         plt.show()
     except FileNotFoundError:
         print(f"File '{pd_csv}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
 
 def main():
     parser = argparse.ArgumentParser(description='Plot worker usage from CSV file')

278f3e6e5467e54f6cdc9594b19e87360b215c14
Norman Koch <norman.koch@tu-dresden.de>
2024-05-26 12:09:31 +0200

    properly check for existance of files in general plot

--- ax/.omniopt_plot_gpu_usage.py
+++ ax/.omniopt_plot_gpu_usage.py
@@ -9,50 +9,27 @@ def dier(msg):
     pprint(msg)
     sys.exit(1)
 
-def remove_duplicate_headers(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-        # Standard-Headerzeile
-        header_line = "timestamp, name, pci.bus_id, driver_version, pstate, pcie.link.gen.max, pcie.link.gen.current, temperature.gpu, utilization.gpu [%], utilization.memory [%], memory.total [MiB], memory.free [MiB], memory.used [MiB]\n"
-
-        return [line.rstrip() for line in lines if not line.startswith("timestamp")]  # Alle Headerzeilen entfernen
-
-    except Exception as e:
-        warn(f"Error occurred while removing duplicate headers: {e}")
-        return None
-
-
-
-
 def plot_gpu_usage(run_dir):
     gpu_data = []
     for root, dirs, files in os.walk(run_dir):
         for file in files:
             if file.startswith("gpu_usage_") and file.endswith(".csv"):
                 file_path = os.path.join(root, file)
-                no_headers = remove_duplicate_headers(file_path)
-                _tmp_file = file_path + "_tmp"
-                with open(_tmp_file, 'w') as file:
-                    file.writelines(no_headers)
                 # Einlesen der Daten mit expliziter Angabe der Spaltennamen
-                df = pd.read_csv(_tmp_file,
+                df = pd.read_csv(file_path,
                                  names=["timestamp", "name", "pci.bus_id", "driver_version", "pstate",
                                         "pcie.link.gen.max", "pcie.link.gen.current", "temperature.gpu",
                                         "utilization.gpu [%]", "utilization.memory [%]", "memory.total [MiB]",
                                         "memory.free [MiB]", "memory.used [MiB]"])
                 gpu_data.append(df)
 
-                os.unlink(_tmp_file)
-
     if not gpu_data:
         print("No GPU usage data found.")
         return
 
-    dier(gpu_data)
     all_gpu_data = pd.concat(gpu_data, ignore_index=True)
     all_gpu_data['timestamp'] = pd.to_datetime(all_gpu_data['timestamp'], format='%Y/%m/%d %H:%M:%S.%f', errors='coerce')
+    all_gpu_data['utilization.gpu [%]'] = all_gpu_data['utilization.gpu [%]'].str.replace('%', '').astype(float)
     all_gpu_data = all_gpu_data.dropna(subset=['timestamp'])  # Entfernen von ungültigen Zeitstempeln
     all_gpu_data.plot(x='timestamp', y='utilization.gpu [%]', kind='line')
     plt.xlabel('Time')

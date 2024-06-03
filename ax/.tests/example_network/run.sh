5d623f9d47a6e7d4720eb8ed62126d25439fe036
Norman Koch <norman.koch@tu-dresden.de>
2024-06-03 20:17:37 +0200

    exit after install, new env

--- ax/.tests/example_network/run.sh
+++ ax/.tests/example_network/run.sh
@@ -85,13 +85,17 @@ if [[ -d $LMOD_DIR ]]; then
 	ml release/23.10 GCC/11.3.0 OpenMPI/4.1.4 TensorFlow/2.11.0-CUDA-11.7.0
 fi
 
-ENV_DIR=$HOME/.test_env
+ENV_DIR=$HOME/.omniopt_test_install_$(uname -m)_$(python3 --version | sed -e 's# #_#g')
 if [[ ! -d "$ENV_DIR" ]]; then
         green_text "$ENV_DIR not found. Creating virtual environment."
         python3 -m venv $ENV_DIR
         source $ENV_DIR/bin/activate
 
         pip install tensorflow tensorflowjs protobuf scikit-image opencv-python keras termcolor pyyaml h5py
+fi
+
+if [[ ! -z $exit_after_install ]]; then
+	exit 0
 fi
 
 source $ENV_DIR/bin/activate

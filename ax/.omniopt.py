59a972c36b8ee90feb22715744ca50c1775b0900
Norman Koch <norman.koch@tu-dresden.de>
2024-06-05 18:36:20 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -1240,27 +1240,6 @@ try:
                 from ax.storage.json_store.save import save_experiment
                 from ax.storage.json_store.load import load_experiment
                 from ax.service.utils.report_utils import exp_to_df
-
-                """
-                class SimpleCustomGP(ExactGP, GPyTorchModel):
-                    _num_outputs = 1  # to inform GPyTorchModel API
-
-                    def __init__(self, train_X, train_Y, train_Yvar: Optional[Tensor] = None):
-                        # NOTE: This ignores train_Yvar and uses inferred noise instead.
-                        # squeeze output dim before passing train_Y to ExactGP
-                        super().__init__(train_X, train_Y.squeeze(-1), GaussianLikelihood())
-                        self.mean_module = ConstantMean()
-                        self.covar_module = ScaleKernel(
-                            base_kernel=RBFKernel(ard_num_dims=train_X.shape[-1]),
-                        )
-                        self.to(train_X)  # make sure we're on the right device/dtype
-
-                    def forward(self, x):
-                        mean_x = self.mean_module(x)
-                        covar_x = self.covar_module(x)
-                        return MultivariateNormal(mean_x, covar_x)
-                """
-
             except ModuleNotFoundError as e:
                 print_color("red", "\n:warning: ax could not be loaded. Did you create and load the virtual environment properly?")
                 exit_local(33)

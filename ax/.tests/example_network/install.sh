c42b1261e9540b2042cc97fecc1d2f6849660d8c
Norman Koch <norman.koch@tu-dresden.de>
2024-06-07 18:57:34 +0200

    fix

--- ax/.tests/example_network/install.sh
+++ ax/.tests/example_network/install.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+FROZEN=""
+
+function ppip {
+	set +e
+	echo "$FROZEN" | grep -i "$1" 2>/dev/null >/dev/null
+	_exit_code=$?
+
+	if [[ "$_exit_code" != "0" ]]; then
+		if [[ "$UPGRADED_PIP" -eq "0" ]]; then
+			pip install --upgrade pip
+			UPGRADED_PIP=1
+		fi
+
+		pip3 install $1 || {
+			red "Failed to install $1. Deleting $VENV_DIR..."
+			rm -rf $VENV_DIR || {
+				red "Failed to delete $VENV_DIR"
+				exit 4
+			}
+
+			exit 3
+		}
+
+		green_text "$1 installed successfully"
+
+		FROZEN=$(pip3 freeze)
+	fi
+	set -e
+}
+
 function echoerr() {
         echo "$@" 1>&2
 }
@@ -26,15 +56,27 @@ if [[ -d $LMOD_DIR ]]; then
 	ml release/23.10 GCC/11.3.0 OpenMPI/4.1.4 TensorFlow/2.11.0-CUDA-11.7.0
 fi
 
-ENV_DIR=$HOME/.omniopt_test_install_$(uname -m)_$(python3 --version | sed -e 's# #_#g')
-if [[ ! -d "$ENV_DIR" ]]; then
-        green_text "$ENV_DIR not found. Creating virtual environment."
-        python3 -m venv $ENV_DIR
-        source $ENV_DIR/bin/activate
-
-        pip install tensorflow tensorflowjs protobuf scikit-image opencv-python keras termcolor pyyaml h5py
-
-	deactivate
+VENV_DIR=$HOME/.omniopt_test_install_$(uname -m)_$(python3 --version | sed -e 's# #_#g')
+if [[ ! -d "$VENV_DIR" ]]; then
+        green_text "$VENV_DIR not found. Creating virtual environment."
+        python3 -m venv $VENV_DIR
 else
-	green_text "$ENV_DIR already exists"
+	green_text "$VENV_DIR already exists"
 fi
+
+source $VENV_DIR/bin/activate || {
+	red "Failed to activate $VENV_DIR"
+	exit 2
+}
+
+FROZEN=$(pip3 freeze)
+
+ppip tensorflow
+ppip tensorflowjs
+ppip protobuf
+ppip scikit-image
+ppip opencv-python
+ppip keras
+ppip termcolor
+ppip pyyaml
+ppip h5py

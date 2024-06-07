07abca315d04b274d5f24cdb239b2f32ee44344e
Norman Koch <norman.koch@tu-dresden.de>
2024-06-07 18:49:38 +0200

    fix

--- ax/.tests/example_network/install.sh
+++ ax/.tests/example_network/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+LMOD_DIR=/software/foundation/$(uname -m)/lmod/lmod/libexec
+
+if [[ -d $LMOD_DIR ]]; then
+	ml () {
+		eval "$($LMOD_DIR/ml_cmd "$@")"
+	}
+
+	ml release/23.10 GCC/11.3.0 OpenMPI/4.1.4 TensorFlow/2.11.0-CUDA-11.7.0
+fi
+
 ENV_DIR=$HOME/.omniopt_test_install_$(uname -m)_$(python3 --version | sed -e 's# #_#g')
 if [[ ! -d "$ENV_DIR" ]]; then
         green_text "$ENV_DIR not found. Creating virtual environment."

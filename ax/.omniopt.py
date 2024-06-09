d78b69c47ca38bd5f4455e41c30ff92e210af5ac
Norman Koch <norman.koch@tu-dresden.de>
2024-06-09 17:43:33 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -3661,6 +3661,7 @@ def test_find_paths(program_code):
 
 def run_tests():
     print_image_to_cli(".tools/slimer.png", 300)
+    print_image_to_cli(".tools/slimer2.png", 300)
 
     nr_errors = 0
 

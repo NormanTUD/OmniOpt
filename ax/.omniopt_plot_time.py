d6156a1fc9ed4fd77cf947e97ffc96c98cbdfb3b
Norman Koch <norman.koch@tu-dresden.de>
2024-06-12 08:04:36 +0200

    added exit code

--- ax/.omniopt_plot_time.py
+++ ax/.omniopt_plot_time.py
@@ -1,6 +1,7 @@
 # DESCRIPTION: Plot time infos
 # EXPECTED FILES: job_infos.csv
 
+import pandas as pd
 import argparse
 import os
 import sys
@@ -48,20 +49,6 @@ def main():
 
     plt.subplots_adjust(hspace=0.4, wspace=0.4)
 
-    """
-    sns.scatterplot(data=df, x='int_param', y='run_time', hue='exit_code', ax=axes[0, 0])
-    axes[0, 0].set_title('Run Time vs. int_param')
-
-    sns.scatterplot(data=df, x='float_param', y='run_time', hue='exit_code', ax=axes[0, 1])
-    axes[0, 1].set_title('Run Time vs. float_param')
-
-    sns.scatterplot(data=df, x='choice_param', y='run_time', hue='exit_code', ax=axes[1, 0])
-    axes[1, 0].set_title('Run Time vs. choice_param')
-
-    sns.scatterplot(data=df, x='int_param_two', y='run_time', hue='exit_code', ax=axes[1, 1])
-    axes[1, 1].set_title('Run Time vs. int_param_two')
-    """
-
     axes[0, 0].hist(df['run_time'], bins=30)
     axes[0, 0].set_title('Distribution of Run Time')
     axes[0, 0].set_xlabel('Run Time')
@@ -72,31 +59,13 @@ def main():
     axes[0, 1].legend()
     axes[0, 1].set_title('Result over Time')
 
-    #sns.countplot(data=df, x='exit_code', ax=axes[1, 0])
-    #axes[1, 0].set_title('Count of Exit Codes')
-
-    #avg_run_time = df.groupby('exit_code')['run_time'].mean().reset_index()
-    #sns.barplot(data=avg_run_time, x='exit_code', y='run_time', ax=axes[1, 0])
-    #axes[1, 0].set_title('Average Run Time by Exit Code')
+    df["exit_code"] = [str(int(x)) for x in df["exit_code"]]
 
     sns.violinplot(data=df, x='exit_code', y='run_time', ax=axes[1, 0])
     axes[1, 0].set_title('Run Time Distribution by Exit Code')
 
-
     sns.boxplot(data=df, x='hostname', y='run_time', ax=axes[1, 1])
     axes[1, 1].set_title('Run Time by Hostname')
-
-
-    """
-    sns.boxplot(data=df, x='exit_code', y='int_param', ax=axes[0, 1])
-    axes[0, 1].set_title('int_param by Exit Code')
-
-    sns.boxplot(data=df, x='exit_code', y='float_param', ax=axes[1, 0])
-    axes[1, 0].set_title('float_param by Exit Code')
-
-    sns.boxplot(data=df, x='exit_code', y='choice_param', ax=axes[1, 1])
-    axes[1, 1].set_title('choice_param by Exit Code')
-    """
 
     plt.show()
 

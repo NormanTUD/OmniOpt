8557dd574248440b3ea87c43a706d38e854efb2b
Norman Koch <norman.koch@tu-dresden.de>
2024-06-16 11:00:17 +0200

    fix

--- ax/.gui/share.php
+++ ax/.gui/share.php
@@ -147,11 +147,9 @@
 
 		if ($added_files) {
 			echo "Run was successfully shared. See https://imageseg.scads.de/omniax/share.php?user=$user_id&experiment=$experiment_name&run_nr=$run_id\nYou can share the link. It is valid for 30 days.\n";
-			echo "\n";
 			exit(0);
 		} else {
 			echo "Error sharing the job. No Files were found.\n";
-			echo "\n";
 			exit(1);
 		}
 	} else {

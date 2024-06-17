43bbdf747ca4047279c53437f81c93f866151c1b
Norman Koch <norman.koch@tu-dresden.de>
2024-06-17 11:38:57 +0200

    fix

--- ax/.gui/share.php
+++ ax/.gui/share.php
@@ -482,6 +482,7 @@
 			} else if (
 				preg_match("/evaluation_errors.log/", $file) || 
 				preg_match("/oo_errors.txt/", $file) ||
+				preg_match("/best_result.txt/", $file) ||
 				preg_match("/get_next_trials/", $file)
 			) {
 				$content = remove_ansi_colors(file_get_contents($file));
@@ -510,7 +511,6 @@
 				preg_match("/state_files/", $file) ||
 				preg_match("/failed_logs/", $file) ||
 				preg_match("/single_runs/", $file) ||
-				preg_match("/best_result.txt/", $file) ||
 				preg_match("/parameters.txt/", $file) ||
 				preg_match("/job_infos.csv/", $file) ||
 				preg_match("/worker_usage.csv/", $file) ||

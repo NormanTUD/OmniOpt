678949cb5f0b17bf2e9ebf455dd4669705ec0075
Norman Koch <norman.koch@tu-dresden.de>
2024-06-25 07:49:21 +0200

    added example

--- ax/.gui/continue_job.php
+++ ax/.gui/continue_job.php
@@ -9,8 +9,16 @@
     
 	<div id="toc"></div>
 
-	<h2 id="script-example">Script Example</h2>
-	TODO
+	<h2 id="continue_with_same_options">Continue an old job with the same options</h2>
+	<p>Continueing an old job with the same options as previously, but with awareness of the hyperparameter-constellations
+	that have already been tested, is as simple as this, assuming your job is in <tt>runs/my_experiment/0</tt>:
+
+	<pre><code class="language-bash">#!/bin/bash -l
+./omniopt --continue runs/my_experiment/0
+</code></pre>
+
+	This will start a new run with the same settings as the old one, but load all already tried out data points, and 
+	continue the search from there.
 
 	<script src="prism.js"></script>
 	<script>

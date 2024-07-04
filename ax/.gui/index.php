57c88a654f0d1e946ee6f032a171f466fc9c42c9
Norman Koch <norman.koch@tu-dresden.de>
2024-07-04 07:29:34 +0200

    fix

--- ax/.gui/index.php
+++ ax/.gui/index.php
@@ -500,7 +500,7 @@
 					var base_64_encoder = value.replaceAll(/"/g, '\\"');
 					log("base_64_encoder:", base_64_encoder);
 
-					value = `$(${base_64_encoder} | base64 -w 0)`;
+					value = `echo "$(${base_64_encoder} | base64 -w 0)"`;
 
 					command += " --" + item.id + "='" + value + "'";
 					$("#" + item.id).css("background-color", "");

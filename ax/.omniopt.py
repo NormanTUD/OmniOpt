2781b2d8cf82997fcd51f46b4a23d6576fc3dc36
Norman Koch <norman.koch@tu-dresden.de>
2024-07-20 18:10:02 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -3074,7 +3074,7 @@ def finish_previous_jobs(args, new_msgs):
 
                 global_vars["jobs"].remove((job, trial_index))
 
-            progress_bar.update(1)
+            #progress_bar.update(1)
 
             if args.verbose:
                 progressbar_description([f"saving checkpoints and {pd_csv_filename}"])
@@ -3340,7 +3340,7 @@ def execute_evaluation(args, trial_index_to_param, ax_client, trial_index, param
             global_vars["jobs"].remove((new_job, trial_index))
             print_debug("Removed failed job")
 
-            progress_bar.update(1)
+            #progress_bar.update(1)
 
             save_checkpoint()
             save_pd_csv()
@@ -4164,7 +4164,7 @@ def main():
         global progress_bar
         progress_bar = _progress_bar
 
-        progress_bar.update(submitted_jobs())
+        progress_bar.update(count_done_jobs())
 
         run_random_jobs(random_steps, ax_client, executor)
 

2030767a27f4d7b860f2bbcff9978259eff39c38
Norman Koch <norman.koch@tu-dresden.de>
2024-07-29 15:26:55 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -3462,7 +3462,7 @@ def get_generation_strategy(num_parallel_jobs, seed, max_eval):
         random_steps = 0
 
     if args.continue_previous_job:
-        random_steps = random_steps _count_sobol_steps(f"{args.continue_previous_job}/results.csv")
+        random_steps = random_steps - _count_sobol_steps(f"{args.continue_previous_job}/results.csv")
 
     if max_eval is None:
         max_eval = max(1, random_steps)

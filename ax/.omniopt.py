a70e96b602f7cc4e780039e0fa4a73ee2fbef539
Norman Koch <norman.koch@tu-dresden.de>
2024-07-29 18:16:14 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -3990,14 +3990,14 @@ def run_random_search(random_steps, ax_client, executor):
     if args.continue_previous_job:
         rand_in_prev_job = _count_sobol_steps(f"{args.continue_previous_job}/results.csv")
 
-    print(f"random_steps {random_steps} + rand_in_prev_job {rand_in_prev_job} >= count_done_jobs() {count_done_jobs()} - nr_inserted_jobs {nr_inserted_jobs} and not search_space_exhausted {search_space_exhausted}")
-    while random_steps + rand_in_prev_job >= (count_done_jobs() - nr_inserted_jobs) and not search_space_exhausted:
-        print(f"random_steps {random_steps} + rand_in_prev_job {rand_in_prev_job} >= count_done_jobs() {count_done_jobs()} - nr_inserted_jobs {nr_inserted_jobs} and not search_space_exhausted {search_space_exhausted}")
+    print(f"random_steps {random_steps} + rand_in_prev_job {rand_in_prev_job} >= count_done_jobs() {count_done_jobs()} - rand_in_prev_job {rand_in_prev_job} and not search_space_exhausted {search_space_exhausted}")
+    while random_steps + rand_in_prev_job >= (count_done_jobs() - rand_in_prev_job) and not search_space_exhausted:
+        print(f"random_steps {random_steps} + rand_in_prev_job {rand_in_prev_job} >= count_done_jobs() {count_done_jobs()} - rand_in_prev_job {rand_in_prev_job} and not search_space_exhausted {search_space_exhausted}")
 
         wait_for_jobs_to_complete(num_parallel_jobs)
 
-        if not args.disable_search_space_exhaustion_detection and count_done_jobs() - nr_inserted_jobs >= max_eval:
-            _wrn = f"run_random_search: searchSpaceExhausted: count_done_jobs() {count_done_jobs()} - nr_inserted_jobs {nr_inserted_jobs} >= max_eval {max_eval}:"
+        if not args.disable_search_space_exhaustion_detection and count_done_jobs() - rand_in_prev_job >= max_eval:
+            _wrn = f"run_random_search: searchSpaceExhausted: count_done_jobs() {count_done_jobs()} - rand_in_prev_job {rand_in_prev_job} >= max_eval {max_eval}:"
 
             progressbar_description(_wrn)
             print_debug(_wrn)
@@ -4006,6 +4006,7 @@ def run_random_search(random_steps, ax_client, executor):
 
         if submitted_jobs() >= random_steps or len(global_vars["jobs"]) == random_steps:
             break
+
         write_process_info()
         write_worker_usage()
 

794fcb0524b17a12200b562e8589f8003966fbec
Norman Koch <norman.koch@tu-dresden.de>
2024-08-09 17:42:14 +0200

    new version

--- ax/.omniopt_plot_scatter.py
+++ ax/.omniopt_plot_scatter.py
@@ -14,6 +14,12 @@ import argparse
 import math
 import signal
 
+textbox_minimum = None
+textbox_maximum = None
+
+maximum_textbox = None
+minimum_textbox = None
+
 #install(show_locals=True)
 install()
 
@@ -663,6 +669,12 @@ def create_widgets():
 
     if helpers.looks_like_float(args.min):
         min_string = str(args.min)
+
+    global textbox_minimum
+    global textbox_maximum
+
+    global maximum_textbox
+    global minimum_textbox
 
     textbox_minimum = plt.axes([0.2, 0.025, 0.1, 0.04])
     minimum_textbox = TextBox(textbox_minimum, 'Minimum result:', initial=min_string)

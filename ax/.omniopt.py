06b4880a0ed9049e9a1042af63e07dc73a3244ad
Norman Koch <norman.koch@tu-dresden.de>
2024-08-11 13:36:32 +0200

    new version

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -23,11 +23,7 @@ except ModuleNotFoundError as e:
     sys.exit(31)
 
 def my_exit(_code=0):
-    tb = traceback.format_exc()
-    try:
-        print_debug(f"Exiting with error code {_code}. Traceback: {tb}")
-    except Exception:
-        print(f"Exiting with error code {_code}. Traceback: {tb}")
+    print_debug(f"Exiting with error code {_code}. Traceback: {tb}")
 
     time.sleep(5)
 

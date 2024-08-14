26e019c20e1b2b98f3450a292888f18469cf84ec
Norman Koch <norman.koch@tu-dresden.de>
2024-08-14 14:18:57 +0200

    new version

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -4077,6 +4077,16 @@ def set_orchestrator():
         else:
             print_yellow("--orchestrator_file will be ignored on non-sbatch-systems.")
 
+def check_if_has_random_steps ():
+    if (not args.continue_previous_job and not args.load_previous_job_data and "--continue" not in sys.argv) and (args.num_random_steps == 0 or not args.num_random_steps):
+        print_red("You have no random steps set. This is only allowed in continued jobs. To start, you need either some random steps, or a continued run.")
+        my_exit(233)
+
+def check_max_eval (max_eval):
+    if not max_eval:
+        print_red("--max_eval needs to be set!")
+        sys.exit(19)
+
 def main():
     print_debug("main()")
 
@@ -4093,9 +4103,7 @@ def main():
     global LOGFILE_DEBUG_GET_NEXT_TRIALS
     global random_steps
 
-    if (not args.continue_previous_job and not args.load_previous_job_data and "--continue" not in sys.argv) and (args.num_random_steps == 0 or not args.num_random_steps):
-        print_red("You have no random steps set. This is only allowed in continued jobs. To start, you need either some random steps, or a continued run.")
-        my_exit(233)
+    check_if_has_random_steps()
 
     _debug_worker_creation("time, nr_workers, got, requested, phase")
 
@@ -4134,9 +4142,7 @@ def main():
 
     disable_logging()
 
-    if not max_eval:
-        print_red("--max_eval needs to be set!")
-        sys.exit(19)
+    check_max_eval(max_eval)
 
     random_steps, second_step_steps = get_number_of_steps(max_eval)
 

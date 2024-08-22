620ad7400bda8e2134c0498424a1ff0a062d1d3f
Norman Koch <norman.koch@tu-dresden.de>
2024-08-22 15:43:05 +0200

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -365,10 +365,10 @@ def print_debug(msg):
     _debug(msg)
 
 def log_system_usage():
-    if not current_run_folder:
+    if not CURRENT_RUN_FOLDER:
         return
 
-    csv_file_path = os.path.join(current_run_folder, "state_files", "cpu_ram_usage.csv")
+    csv_file_path = os.path.join(CURRENT_RUN_FOLDER, "state_files", "cpu_ram_usage.csv")
 
     os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
 

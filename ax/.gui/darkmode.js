af12114df48c85d87fc4dfccf11909e588b9a84d
Norman Koch <norman.koch@tu-dresden.de>
2024-09-03 09:52:56 +0200

    fix

--- ax/.gui/darkmode.js
+++ ax/.gui/darkmode.js
@@ -1,11 +1,13 @@
 function enable_dark_mode() {
 	$('html').css('filter', 'invert(1)');
 	$('.invert_in_dark_mode').css('filter', 'invert(1)');
+	$('img').css('filter', 'invert(1)');
 }
 
 function enable_light_mode() {
 	$('html').css('filter', '');
 	$('.invert_in_dark_mode').css('filter', '');
+	$('img').css('filter', 'invert(0)');
 }
 
 function apply_theme_based_on_system_preferences() {
@@ -15,10 +17,6 @@ function apply_theme_based_on_system_preferences() {
 		enable_light_mode();
 	}
 }
-
-// Apply the theme when the page loads
-$(document).ready(function() {
-});
 
 // Listen for changes to the system color scheme preferences
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {

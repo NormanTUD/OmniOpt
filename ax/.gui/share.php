22b635bd8f9ec978f4b758d5d8534b8f9b279427
Norman Koch <norman.koch@tu-dresden.de>
2024-09-12 11:20:04 +0200

    fix

--- ax/.gui/share.php
+++ ax/.gui/share.php
@@ -14,34 +14,29 @@
 		</div>
 	</div>
 	<script>
-		$(document).ready(function() {
-			// Die aktuellen URL-Parameter holen
+		function load_content() {
 			var queryString = window.location.search;
 
-			// Die URL für die AJAX-Anfrage erstellen, indem die aktuellen Parameter angehängt werden
 			var requestUrl = 'share_internal.php' + queryString;
 
-			// AJAX-Anfrage starten, um den Inhalt von 'share_internal.php' zu laden
 			$.ajax({
 			url: requestUrl,
 				method: 'GET',
 				success: function(response) {
-					// Den Inhalt von 'share_internal.php' in das div #share_main schreiben
 					$('#share_main').html(response);
-
-					// Das div loading_screen verstecken
 					$('#loading_screen').hide();
-
-					// Das div #share_main anzeigen
 					$('#share_main').show();
 				},
 				error: function() {
-					// Fehlerbehandlung, falls die Anfrage fehlschlägt
 					console.error('Error loading the content.');
 					$('#loading_screen').hide();
 					$('#share_main').html('Error loading the requested content!').show();
 				}
 			});
+		}		
+
+		$(document).ready(function() {
+			load_content();
 		});
 	</script>
 </body>

d6664fa5256f2bc73b353c3c0a867a77a45b3df6
Norman Koch <norman.koch@tu-dresden.de>
2024-10-06 19:13:10 +0200

    fix

--- ax/.gui/delete_old_data.php
+++ ax/.gui/delete_old_data.php
@@ -6,24 +6,17 @@
 		$currentTime = time();
 		$threshold = 30 * 24 * 60 * 60; // 30 Tage in Sekunden
 
-		// Durchlaufe alle Unterordner in der angegebenen Struktur
 		foreach (glob("$dir/*/*/*", GLOB_ONLYDIR) as $subdir) {
-			// Prüfe das Alter des Ordners
 			$dir_date = filemtime($subdir);
 			if (is_dir($subdir) && ($dir_date < ($currentTime - $threshold))) {
-				// Ordner ist älter als 30 Tage
-				// Füge den Ordner zur Liste der zu löschenden Ordner hinzu
 				$oldDirectories[] = $subdir;
-
-				// Kommentierter Code, um den Ordner zu löschen:
-				// rmdir($subdir);
 			}
 		}
 
 		return $oldDirectories;
 	}
 
-	$directoryToCheck = 'shares'; // Setze hier den Hauptordner
+	$directoryToCheck = 'shares';
 	$oldDirs = checkOldDirectories($directoryToCheck);
 
 	echo json_encode($oldDirs);

65294f83088f4fa7b843cb26cab968b9c519df6d
Norman Koch <norman.koch@tu-dresden.de>
2024-10-06 19:33:35 +0200

    fix

--- ax/.gui/delete_old_data.php
+++ ax/.gui/delete_old_data.php
@@ -4,21 +4,27 @@
 	function checkOldDirectories($dir) {
 		$oldDirectories = [];
 		$currentTime = time();
-		$threshold = 30 * 24 * 60 * 60; // 30 Tage in Sekunden
 
+		// Durchlaufe alle Unterordner in der Struktur
 		foreach (glob("$dir/*/*/*", GLOB_ONLYDIR) as $subdir) {
-			$dir_date = filemtime($subdir);
+			$pathParts = explode('/', $subdir); // Zerlege den Pfad in seine Komponenten
+			$secondDir = $pathParts[1] ?? ''; // Finde den zweiten Ordner im Pfad (index 1)
+
+			// Setze das Zeitlimit auf 2 Stunden, wenn der zweite Ordner "runner" ist, ansonsten auf 30 Tage
+			$threshold = ($secondDir === 'runner') ? 2 * 60 * 60 : 30 * 24 * 60 * 60; // 2 Stunden oder 30 Tage
+
+			$dir_date = filemtime($subdir); // Hol dir das Änderungsdatum des Ordners
 			if (is_dir($subdir) && ($dir_date < ($currentTime - $threshold))) {
-				$oldDirectories[] = $subdir;
+				$oldDirectories[] = $subdir; // Füge alte Ordner der Liste hinzu
 			}
 		}
 
 		return $oldDirectories;
 	}
 
-	$directoryToCheck = 'shares';
+	$directoryToCheck = 'shares'; // Wurzelverzeichnis
 	$oldDirs = checkOldDirectories($directoryToCheck);
 
-	echo json_encode($oldDirs);
+	echo json_encode($oldDirs, JSON_PRETTY_PRINT); // Ausgabe der alten Verzeichnisse als JSON mit Formatierung
 	echo "\n";
 ?>

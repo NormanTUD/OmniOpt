98e3984b1cebbec8edcb27791ab31767e823df65
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 13:26:51 +0200

    fix

--- ax/.gui/delete_old_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("share_functions.php");
-
-	$oldDir = delete_old_shares();
-
-	header('Content-Type: application/json');
-	echo json_encode($oldDirs, JSON_PRETTY_PRINT);
-	echo "\n";
-?>

4533c629160d4841c6e6c2e983699e4986dabb55
Norman Koch <norman.koch@tu-dresden.de>
2024-10-22 18:59:57 +0200

    fix

--- ax/.gui/share_graphs.js
+++ ax/.gui/share_graphs.js
@@ -620,7 +620,7 @@ async function load_out_files () {
 				var _new_tab_id = `out_files_${md5(_d.data + _fn)}`;
 				if($("#" + _new_tab_id).length == 0) {
 					showSpinnerOverlay(`Loading log ${_fn}...`);
-					var _new_tab_title = `${_fn} <span class='invert_in_dark_mode'>${get_checkmark_if_contains_result(_d.data)}</span>`;
+					var _new_tab_title = `${_fn.replace("_0_log.out", "")} <span class='invert_in_dark_mode'>${get_checkmark_if_contains_result(_d.data)}</span>`;
 					var _new_tab_content = `<div class='out_file_internal' id='out_file_content_${md5(_d.data + _fn)}_internal'><pre style='color: lightgreen; background-color: black;' class='invert_in_dark_mode'>${_d.data}</pre></div>`;
 
 					add_tab(_new_tab_id, _new_tab_title, _new_tab_content, "#" + main_tabs_div_id);

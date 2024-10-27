d5464648ea64c3f60b2da9698e1def672655ce4d
Norman Koch <norman.koch@tu-dresden.de>
2024-10-27 14:56:15 +0100

    fix

--- ax/setup.py
+++ ax/setup.py
@@ -113,7 +113,7 @@ setup(
     author_email='norman.koch@tu-dresden.de',
     url='https://scads.ai/transfer-2/verfuegbare-software-dienste-en/omniopt/',
     install_requires=install_requires,
-    packages=['omniopt2',],
+    packages=['.',],
     data_files=[('bin', all_needed_files)],
     include_package_data=True,
 )

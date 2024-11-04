10b3f11abfaba930096ca5155a159f530c56cd13
Norman Koch <norman.koch@tu-dresden.de>
2024-11-04 14:55:45 +0100

    fix

---
+++ ax/.gui/tutorials/partitions.php
@@ -0,0 +1,9 @@
+<h1>Available partitions</h1>
+
+<div id="toc"></div>
+
+<h2 id="available_partitions">Available Partitions</h2>
+
+<?php
+	echo createPartitionOverviewTable("partition_data.json");
+?>

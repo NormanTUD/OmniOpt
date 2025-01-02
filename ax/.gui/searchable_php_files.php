e78746a7f6ad571547591b1bbdc28425a67519b3
n <norman.koch@tu-dresden.de>
2025-01-02 09:04:57 +0100

    fix

--- ax/.gui/searchable_php_files.php
+++ ax/.gui/searchable_php_files.php
@@ -1,22 +1,29 @@
 <?php
-    require_once "_functions.php";
+	require_once "_functions.php";
 
-    $GLOBALS["files"] = array(
-        "tutorials" => array(
-            "name" => "Tutorials&Help",
-            "entries" => array(
-                "basics" => "Basic info on OmniOpt2",
-                "run_sh" => "Create <tt>run.sh</tt>-file &amp; modify your program",
-                "folder_structure" => "Folder Structure of OmniOpt2 runs",
-                "plot" => "Plot your results",
-                "continue_job" => "Continue jobs",
-                "oo_share" => "OmniOpt2-Share",
-                "help" => "Parameters",
-                "debug" => "Debug",
-                "exit_codes_and_bash_scripting" => "Exit-Codes and Bash-scripting"
-            )
-        ),
-        "gui" => "GUI",
-        "share" => "Share",
-        "usage_stats" => "Statistics"
-    );
+	$GLOBALS["files"] = array(
+		"tutorials" => array(
+			"name" => "Tutorials&Help",
+			"entries" => array()
+		),
+		"gui" => "GUI",
+		"share" => "Share",
+		"usage_stats" => "Statistics"
+	);
+
+	$_files = scandir('tutorials/');
+	foreach ($_files as $file) {
+		if ($file != ".." && $file != "." && $file != "favicon.ico" and preg_match("/\.php/", $file)) {
+			$name = $file;
+
+			$heading_content = get_first_heading_content("tutorials/$file");
+
+			if ($heading_content !== null) {
+				$name = $heading_content;
+			}
+
+			$file = preg_replace("/\.php$/", "", $file);
+
+			$GLOBALS["files"]["tutorials"]["entries"][$file] = $name;
+		}
+	}
--- ax/.gui/tutorials.php
+++ ax/.gui/tutorials.php
@@ -1,24 +1,6 @@
 <?php
 	require "_header_base.php";
 
-	function get_first_heading_content($file_path) {
-		// Read the content of the file
-		$file_content = file_get_contents($file_path);
-
-		if ($file_content === false) {
-			return null; // Return null if the file could not be read
-		}
-
-		// Define a regular expression to match the first <h1> to <h6> tag and capture its content
-		$heading_pattern = '/<h[1-6][^>]*>(.*?)<\/h[1-6]>/i';
-
-		// Search for the first matching heading tag
-		if (preg_match($heading_pattern, $file_content, $matches)) {
-			return $matches[1]; // Return the captured content of the first heading tag
-		}
-
-		return null; // Return null if no heading tag was found
-	}
 	if (isset($_GET["tutorial"])) {
 		$tutorial_file = $_GET["tutorial"];
 		if (preg_match("/^[a-z_]+$/", $tutorial_file)) {

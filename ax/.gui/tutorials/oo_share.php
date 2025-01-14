cac99b956775234c5a9d464091b0181d93a57dd9
n <norman.koch@tu-dresden.de>
2025-01-14 15:30:42 +0100

    fix

--- ax/.gui/tutorials/oo_share.php
+++ ax/.gui/tutorials/oo_share.php
@@ -5,7 +5,7 @@
 <h2 id="what_is_oo_share">What is OmniOpt2-Share?</h2>
 
 <p>OmniOpt2-Share allows you to Share your results with others, online. You can simply submit a job by
-<code class="language-bash">./omniopt_share runs/my_experiment/0</code>. The program will upload the
+<span class="invert_in_dark_mode"><code class="language-bash">./omniopt_share runs/my_experiment/0</code></span>. The program will upload the
 job to our server, and allow give you a link to it which is valid for 30 days.</p>
 
 <h2 class="privacy">Notes on Privacy</h2>

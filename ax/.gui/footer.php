cf21732651ffe1a92b593653e5a9eec9dd590550
n <norman.koch@tu-dresden.de>
2025-01-14 13:04:01 +0100

    fix

--- ax/.gui/footer.php
+++ ax/.gui/footer.php
@@ -1,6 +1,6 @@
 		<footer style="height: 80px; background-color: #f8f9fa; padding: 10px; text-align: center; border-top: 1px solid #dee2e6; width: 100%; box-sizing: border-box; position: fixed; bottom: 0;">
 			<p>
-				&copy; <?php echo date('Y'); ?> OmniOpt2/ScaDS |
+				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2/ScaDS |
 				<a href="https://scads.ai/imprint/" target="_blank" style="color: #007bff; text-decoration: none;">Imprint/Impressum</a> |
 				<a href="https://scads.ai/privacy/" target="_blank" style="color: #007bff; text-decoration: none;">Privacy Policy</a> |
 				<a href="mailto:norman.koch@scads.ai" style="color: #007bff; text-decoration: none;">Contact Us</a>

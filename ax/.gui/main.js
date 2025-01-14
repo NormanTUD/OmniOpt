35218606aa3aa6d2302579ed800aa2128bc3d0e8
n <norman.koch@tu-dresden.de>
2025-01-14 16:56:50 +0100

    fix

--- ax/.gui/main.js
+++ ax/.gui/main.js
@@ -381,8 +381,8 @@ function create_table_from_csv_data(csvData, table_container, new_table_id, opti
 	$(document).ready(function() {
 		var _new_table_id = `#${new_table_id}`;
 		$(_new_table_id).DataTable({
-			"ordering": true,  // Enable sorting on all columns
-			"paging": false    // Show all entries on one page (no pagination)
+			"ordering": true,
+			"paging": false
 		});
 	});
 }

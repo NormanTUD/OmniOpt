dd4b138aae3409af29e3e4472e82522767c91184
n <norman.koch@tu-dresden.de>
2025-01-14 13:29:41 +0100

    fix

--- ax/.gui/footer.php
+++ ax/.gui/footer.php
@@ -1,9 +1,9 @@
 		<footer style="height: 80px; background-color: #f8f9fa; padding: 10px; text-align: center; border-top: 1px solid #dee2e6; width: 100%; box-sizing: border-box; position: fixed; bottom: 0;">
 			<p>
-				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2/<a style="color: #007bff; text-decoration: none;" href="https://scads.ai/" target="_blank">ScaDS</a> |
-				<a href="https://scads.ai/imprint/" target="_blank" style="color: #007bff; text-decoration: none;">Imprint/Impressum</a> |
-				<a href="https://scads.ai/privacy/" target="_blank" style="color: #007bff; text-decoration: none;">Privacy Policy</a> |
-				<a href="mailto:norman.koch@scads.ai" style="color: #007bff; text-decoration: none;">Contact Us</a>
+				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2/<a href="https://scads.ai/" target="_blank">ScaDS</a> |
+				<a href="https://scads.ai/imprint/" target="_blank">Imprint/Impressum</a> |
+				<a href="https://scads.ai/privacy/" target="_blank">Privacy Policy</a> |
+				<a href="mailto:norman.koch@scads.ai">Contact Us</a>
 			</p>
 		</footer>
 	</body>

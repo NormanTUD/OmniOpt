85499328955bbdd0a968a53862264c4355379b57
Norman Koch <norman.koch@tu-dresden.de>
2025-01-31 12:45:59 +0100

    fix

--- ax/.gui/_header_base.php
+++ ax/.gui/_header_base.php
@@ -100,6 +100,7 @@
 
 			document.addEventListener('keydown', handleKeyDown);
 		</script>
+		<script id="MathJax-script" async src="tex-mml-chtml.js"></script>
 	</head>
 	<body>
 		<div id="scads_bar">

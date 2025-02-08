ce8906cbafded601c83b289d38e93615e1d84160
n <norman.koch@tu-dresden.de>
2025-02-08 12:38:52 +0100

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -5371,6 +5371,7 @@ def get_next_nr_steps(_num_parallel_jobs: int, _max_eval: int) -> int: # pragma:
 
     table.add_row("max_eval", str(max_eval))
     table.add_row("_max_eval", str(_max_eval))
+    table.add_row("submitted_jobs()", str(submitted_jobs()))
     table.add_row("simulated_nr_inserted_jobs", str(simulated_nr_inserted_jobs))
     table.add_row("max_eval_plus_inserted", str(max_eval_plus_inserted))
     table.add_row("num_parallel_jobs_minus_existing_jobs", str(num_parallel_jobs_minus_existing_jobs))

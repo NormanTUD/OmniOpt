b566cf497a2a92017c62c579a768772ff75c8302
n <norman.koch@tu-dresden.de>
2025-02-14 18:15:25 +0100

    fix

--- ax/.gui/footer.js
+++ ax/.gui/footer.js
@@ -1,4 +1,38 @@
 "use strict";
+
 $(document).ready(function() {
 	generateTOC();
 });
+
+async function start_gremlins () {
+	(function() {
+		function callback() {
+			var horde = gremlins.createHorde({
+				species: [
+					gremlins.species.clicker(),
+					gremlins.species.toucher(),
+					gremlins.species.formFiller(),
+					gremlins.species.scroller(),
+					gremlins.species.typer()
+				],
+				mogwais: [
+					gremlins.mogwais.alert(),
+					gremlins.mogwais.gizmo()
+				],
+				strategies: [
+					gremlins.strategies.distribution()
+				]
+			});
+
+			horde.unleash();
+		}
+		var s = document.createElement("script");
+		s.src = "https://unpkg.com/gremlins.js";
+		if (s.addEventListener) {
+			s.addEventListener("load", callback, false);
+		} else if (s.readyState) {
+			s.onreadystatechange = callback;
+		}
+		document.body.appendChild(s);
+	})();
+}
--- ax/.gui/gui.js
+++ ax/.gui/gui.js
@@ -1140,36 +1140,3 @@ function copy_bashcommand_to_clipboard_curl () {
 		$("#copied_curl").fadeOut();
 	}, 5000);
 }
-
-async function start_gremlins () {
-	(function() {
-		function callback() {
-			var horde = gremlins.createHorde({
-				species: [
-					gremlins.species.clicker(),
-					gremlins.species.toucher(),
-					gremlins.species.formFiller(),
-					gremlins.species.scroller(),
-					gremlins.species.typer()
-				],
-				mogwais: [
-					gremlins.mogwais.alert(),
-					gremlins.mogwais.gizmo()
-				],
-				strategies: [
-					gremlins.strategies.distribution()
-				]
-			});
-
-			horde.unleash();
-		}
-		var s = document.createElement("script");
-		s.src = "https://unpkg.com/gremlins.js";
-		if (s.addEventListener) {
-			s.addEventListener("load", callback, false);
-		} else if (s.readyState) {
-			s.onreadystatechange = callback;
-		}
-		document.body.appendChild(s);
-	})();
-}

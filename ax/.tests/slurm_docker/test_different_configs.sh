81f28335ddc9aa162b1a99746406f7e8427cf253
Norman Koch <norman.koch@tu-dresden.de>
2025-02-18 08:11:06 +0100

    fix

--- ax/.tests/slurm_docker/test_different_configs.sh
+++ ax/.tests/slurm_docker/test_different_configs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for max_eval in $(seq 1 30); do
-	for num_parallel_jobs in $(seq 1 30); do
-		for num_random_steps in $(seq 1 30); do
+min=1
+max=10
+stepsize=2
+
+for max_eval in $(seq $min $stepsize $max); do
+	for num_parallel_jobs in $(seq $min $stepsize $max); do
+		for num_random_steps in $(seq $min $stepsize $max); do
 			echo "max_eval: $max_eval - num_parallel_jobs: $num_parallel_jobs - num_random_steps: $num_random_steps"
 			bash run_docker --num_random_steps=$num_random_steps --max_eval=$max_eval --num_parallel_jobs=$num_parallel_jobs || {
 				echo ">>>bash run_docker --num_random_steps=$num_random_steps --max_eval=$max_eval --num_parallel_jobs=$num_parallel_jobs<<< failed with $?"

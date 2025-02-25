b2fdbb251a93db326c72a5c5bb8144b2313894b3
Norman Koch <norman.koch@tu-dresden.de>
2025-02-25 09:02:59 +0100

    fix

--- ax/.gui/_usage_stats_header.php
+++ ax/.gui/_usage_stats_header.php
@@ -176,14 +176,14 @@
 								name: 'Top Users by Number of Jobs'
 							};
 
-							Plotly.newPlot('$element_id-exit-codes', [exitCodePlot], {title: 'Exit Codes', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-runs', [userPlot], {title: 'Runs per User', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-runtimes', [runtimePlot], {title: 'Runtimes', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-runtime-vs-exit-code', [runtimeVsExitCodePlot], {title: 'Runtime vs Exit Code', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-exit-code-pie', [exitCodePie], {title: 'Exit Code Distribution', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-avg-runtime-bar', [avgRuntimeBar], {title: 'Average Runtime per Exit Code', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-runtime-box', [runtimeBox], {title: 'Runtime Distribution', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
-							Plotly.newPlot('$element_id-top-users', [topUserBar], {title: 'Top Users by Number of Jobs', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)'});
+							Plotly.newPlot('$element_id-exit-codes', [exitCodePlot], {title: 'Exit Codes', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
+							Plotly.newPlot('$element_id-runs', [userPlot], {title: 'Runs per User', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
+							Plotly.newPlot('$element_id-runtimes', [runtimePlot], {title: 'Runtimes', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
+							Plotly.newPlot('$element_id-runtime-vs-exit-code', [runtimeVsExitCodePlot], {title: 'Runtime vs Exit Code', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
+							Plotly.newPlot('$element_id-exit-code-pie', [exitCodePie], {title: 'Exit Code Distribution', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0), width: 800'});
+							Plotly.newPlot('$element_id-avg-runtime-bar', [avgRuntimeBar], {title: 'Average Runtime per Exit Code', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
+							Plotly.newPlot('$element_id-runtime-box', [runtimeBox], {title: 'Runtime Distribution', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
+							Plotly.newPlot('$element_id-top-users', [topUserBar], {title: 'Top Users by Number of Jobs', paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0,0,0,0)', width: 800});
 
 							if ($show_sbatch_plot) {
 								var sbatchPlot = {

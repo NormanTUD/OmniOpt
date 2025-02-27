dfc959eafd62df07433264fc531ecc45876851ca
n <norman.koch@tu-dresden.de>
2025-02-27 18:17:10 +0100

    fix

--- ax/.omniopt.py
+++ ax/.omniopt.py
@@ -3309,6 +3309,35 @@ def show_end_table_and_save_end_files(csv_file_path: str) -> int:
     return _exit
 
 @beartype
+def abandon_job(job: Job, trial_index: int) -> bool: # pragma: no cover
+    global global_vars
+
+    if job:
+        try:
+            if ax_client:
+                _trial = ax_client.get_trial(trial_index)
+                _trial.mark_abandoned()
+                global_vars["jobs"].remove((job, trial_index))
+            else:
+                print_red("ax_client could not be found")
+                my_exit(9)
+        except Exception as e: # pragma: no cover
+            print(f"ERROR in line {get_line_info()}: {e}")
+            print_debug(f"ERROR in line {get_line_info()}: {e}")
+            return False
+        job.cancel()
+        return True
+
+    return False
+
+@beartype
+def abandon_all_jobs() -> None: # pragma: no cover
+    for job, trial_index in global_vars["jobs"][:]:
+        abandoned = abandon_job(job, trial_index)
+        if not abandoned:
+            print_debug(f"Job {job} could not be abandoned.")
+
+@beartype
 def end_program(csv_file_path: str, _force: Optional[bool] = False, exit_code: Optional[int] = None) -> None:
     global global_vars, END_PROGRAM_RAN
 
@@ -3353,6 +3382,8 @@ def end_program(csv_file_path: str, _force: Optional[bool] = False, exit_code: O
             _exit = new_exit
     except TypeError as e: # pragma: no cover
         print_red(f"\n⚠ The program has been halted without attaining any results. Error: {e}")
+
+    abandon_all_jobs()
 
     save_pd_csv()
 
@@ -5002,15 +5033,12 @@ def finish_job_core(job: Any, trial_index: int, this_jobs_finished: int) -> int:
                 progressbar_description([f"new result: {result} (entered)"])
             except Exception as e: # pragma: no cover
                 print(f"ERROR in line {get_line_info()}: {e}")
-
-            global_vars["jobs"].remove((job, trial_index))
         else:
             print_debug(f"Counting job {job} as failed, because the result is {result}")
             if job:
                 try:
                     progressbar_description(["job_failed"])
-
-                    ax_client.log_trial_failure(trial_index=trial_index, unsafe=True)
+                    ax_client.log_trial_failure(trial_index=trial_index)
                 except Exception as e: # pragma: no cover
                     print(f"ERROR while trying to mark job as failure in line {get_line_info()}: {e}")
                 job.cancel()
@@ -5021,6 +5049,7 @@ def finish_job_core(job: Any, trial_index: int, this_jobs_finished: int) -> int:
     else: # pragma: no cover
         print_red("ax_client could not be found or used")
         my_exit(9)
+    global_vars["jobs"].remove((job, trial_index))
 
     return this_jobs_finished
 
@@ -5068,6 +5097,7 @@ def finish_previous_jobs(new_msgs: List[str]) -> None:
                     orchestrate_job(job, trial_index)
                 failed_jobs(1)
                 this_jobs_finished += 1
+                global_vars["jobs"].remove((job, trial_index))
             save_checkpoint()
             save_pd_csv()
         else: # pragma: no cover
@@ -5474,6 +5504,7 @@ def cancel_failed_job(trial_index: int, new_job: Job) -> None: # pragma: no cove
         new_job.cancel()
         print_debug("Cancelled failed job")
 
+        global_vars["jobs"].remove((new_job, trial_index))
         print_debug("Removed failed job")
         save_checkpoint()
         save_pd_csv()
@@ -6363,6 +6394,8 @@ def wait_for_jobs_to_complete() -> None: # pragma: no cover
             _sleep(5)
 
         finish_previous_jobs([f"waiting for jobs ({len(global_vars['jobs'])} left)"])
+
+        clean_completed_jobs()
 
 @beartype
 def human_readable_generation_strategy() -> Optional[str]:

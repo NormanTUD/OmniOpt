75acd157edaf1467424adf7b11789af641b1c7d2
Norman Koch <norman.koch@tu-dresden.de>
2025-03-01 11:01:04 +0100

    fix

--- ax/.gui/share_graphs.js
+++ ax/.gui/share_graphs.js
@@ -659,9 +659,7 @@ async function load_out_files () {
 						var _new_tab_title = `${_fn.replace("_0_log.out", "")} <span>${get_checkmark_if_contains_result(_d.data, result_names)}</span>`;
 						var _new_tab_content =
 							`<div class='out_file_internal' id='out_file_content_${md5(_d.data + _fn)}_internal'>
-								<pre style='color: lightgreen; background-color: black;' class='invert_in_dark_mode'>
-									${ansi_to_html(_d.data)}
-								</pre>
+								<pre style='color: lightgreen; background-color: black;' class='invert_in_dark_mode'>${ansi_to_html(_d.data)}</pre>
 							</div>
 						`;
 

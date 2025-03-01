1f426515bcaadee6924d02ba8824c2d1eb7c0176
n <norman.koch@tu-dresden.de>
2025-03-01 16:56:14 +0100

    fix

--- ax/.gui/share_graphs.js
+++ ax/.gui/share_graphs.js
@@ -4,12 +4,16 @@ var hashcache = [];
 var max_nr_ticks = 1000;
 
 function add_load_worker_cpu_button () {
-	$("#main_tabbed").after("<button id='load_worker_cpu_button' class='invert_in_dark_mode' style='width: 300px' onclick='plot_worker_cpu_ram()'>Load Worker/CPU graphs (slow)</button>\n");
+	if (!("#load_worker_cpu_button").length) {
+		$("#main_tabbed").after("<button id='load_worker_cpu_button' class='invert_in_dark_mode' style='width: 300px' onclick='plot_worker_cpu_ram()'>Load Worker/CPU graphs (slow)</button>\n");
+	}
 	apply_theme_based_on_system_preferences();
 }
 
 function add_load_log_button () {
-	$("#main_tabbed").after("<button id='load_debug_log_button' class='invert_in_dark_mode' style='width: 300px' onclick='load_debug_log()'>Load debug log</button>\n");
+	if (!("#load_debug_log_button").length) {
+		$("#main_tabbed").after("<button id='load_debug_log_button' class='invert_in_dark_mode' style='width: 300px' onclick='load_debug_log()'>Load debug log</button>\n");
+	}
 	apply_theme_based_on_system_preferences();
 }
 

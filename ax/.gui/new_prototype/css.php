7236c29fb84bd95ce40b86cd0979844943310e3a
n <norman.koch@tu-dresden.de>
2025-03-04 07:22:44 +0100

    fix

--- ax/.gui/new_prototype/css.php
+++ ax/.gui/new_prototype/css.php
@@ -1,8 +1,10 @@
 <?php
-	$theme = "xp";
+	$theme = "https://unpkg.com/xp.css";
 
 	if($_GET["theme"] == 98) {
-		$theme = "98";
+		$theme = "https://unpkg.com/98.css";
+	} else if($_GET["theme"] == 7) {
+		$theme = "7.css";
 	}
 ?>
-<link rel="stylesheet" href="https://unpkg.com/<?php print $theme; ?>.css">
+<link rel="stylesheet" href="<?php print $theme; ?>">

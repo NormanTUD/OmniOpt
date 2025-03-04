f914e06a56bcfc176062148529375e20e4aa3671
Norman Koch <norman.koch@tu-dresden.de>
2025-03-04 09:26:41 +0100

    fix

--- ax/.gui/new_prototype/css.php
+++ ax/.gui/new_prototype/css.php
@@ -40,12 +40,14 @@
 	}
 </style>
 <?php
-	$theme = "https://unpkg.com/xp.css";
+	$theme = "xp.css";
 
 	if($_GET["theme"] == 98) {
-		$theme = "https://unpkg.com/98.css";
+		$theme = "98.css";
 	} else if($_GET["theme"] == 7) {
 		$theme = "7.css";
+	} else if($_GET["theme"] == "nes") {
+		$theme = "nes.css";
 	} else if($_GET["theme"] == "none") {
 		$theme = "none";
 	}

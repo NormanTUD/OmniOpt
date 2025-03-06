aae403af3109794357e7e19028aeb7641c3a66d4
n <norman.koch@tu-dresden.de>
2025-03-06 20:03:42 +0100

    second step in moving new share to old share

---
+++ ax/.gui/css/share_css.php
@@ -0,0 +1,134 @@
+<style>
+	body.dark-mode {
+		background-color: #1e1e1e; color: #fff;
+	}
+
+	.plot-container {
+		margin-bottom: 2rem;
+	}
+
+	.spinner {
+		border: 4px solid #f3f3f3;
+		border-top: 4px solid #3498db;
+		border-radius: 50%;
+		width: 40px;
+		height: 40px;
+		animation: spin 2s linear infinite;
+		margin: auto;
+	}
+
+	@keyframes spin {
+		0% { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
+	}
+
+	.tabs {
+		margin-bottom: 20px;
+	}
+
+	.tab-content {
+		display: none;
+	}
+
+	.tab-content.active {
+		display: block;
+	}
+
+	pre {
+		color: #00CC00 !important;
+		background-color: black !important;
+		font-family: monospace !important;
+		line-break: anywhere;
+	}
+
+	menu[role="tablist"] {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 4px;
+		max-width: 100%;
+		max-height: 100px;
+		overflow: scroll;
+	}
+
+	menu[role="tablist"] button {
+		white-space: nowrap;
+		min-width: 100px;
+	}
+
+	.container {
+		max-width: 100% !important;
+	}
+
+	.gridjs-sort {
+		min-width: 1px !important;
+	}
+
+	td.gridjs-td {
+		overflow: clip;
+	}
+
+	.gridjs-input {
+		font-family: unset;
+	}
+
+	.gridjs-pages>button {
+		font-family: unset;
+	}
+
+	.title-bar-text {
+		font-size: 22px;
+		display: block ruby;
+	}
+
+	.title-bar {
+		height: fit-content;
+	}
+
+	.window {
+		width: fit-content;
+		min-width: 100%;
+	}
+
+	.top_link {
+		display: inline-block;
+		padding: 5px 5px;
+		background-color: #007bff; /* Blau, kannst du anpassen */
+		color: white;
+		text-decoration: none;
+		font-size: 16px;
+		font-weight: bold;
+		border-radius: 6px;
+		border: 2px solid #0056b3;
+		text-align: center;
+		transition: all 0.3s ease-in-out;
+	}
+
+	.top_link:hover {
+		background-color: #0056b3;
+		border-color: #004494;
+	}
+
+	.top_link:active {
+		background-color: #003366;
+		border-color: #002244;
+	}
+
+	button {
+		color: black;
+		font-family: -apple-system, ".SFNSDisplay-Regular", "Helvetica Neue", Helvetica, Arial, sans-serif
+	}
+</style>
+<?php
+	$theme = "xp.css";
+
+
+	if(isset($_GET["theme"]) && $_GET["theme"] == "none") {
+		$theme = "none";
+	}
+
+	if ($theme != "none") {
+?>
+		<link rel="stylesheet" href="css/<?php print $theme; ?>">
+<?php
+	}
+?>
---
+++ ax/.gui/get_log.php
@@ -0,0 +1,26 @@
+<?php
+	include_once("share2_functions.php");
+
+	try {
+		$run_nr = validate_param("run_nr", "/^\d+$/", "Invalid run_nr");
+		$user_id = validate_param("user_id", "/^[a-zA-Z0-9_]+$/", "Invalid user_id");
+		$experiment_name = validate_param("experiment_name", "/^[a-zA-Z0-9_-]+$/", "Invalid experiment_name");
+		$filename = validate_param("filename", "/^[a-zA-Z0-9\._]+$/", "Invalid filename");
+		$run_folder_without_shares = build_run_folder_path($user_id, $experiment_name, $run_nr);
+		$run_folder = $GLOBALS["sharesPath"]."/$run_folder_without_shares";
+
+		validate_directory($run_folder);
+
+		$path = "$run_folder/$filename";;
+
+		if(file_exists($path)) {
+			$out_file = ansi_to_html(file_get_contents($path));
+			$out_file = highlightDebugInfo($out_file);
+			respond_with_json($out_file);
+		} else {
+			respond_with_error("Invalid path $path found");
+		}
+	} catch (Exception $e) {
+		respond_with_error($e->getMessage());
+	}
+?>

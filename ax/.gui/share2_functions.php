5dc32cce9545b16cc552ff8e95398e515555dcc4
n <norman.koch@tu-dresden.de>
2025-03-07 15:46:02 +0100

    fix

--- ax/.gui/share2_functions.php
+++ ax/.gui/share2_functions.php
@@ -453,8 +453,8 @@
         - <code>x̄</code> and <code>ȳ</code> are the means of the two variables.
     </p>
     <ul>
-        <li><strong>Large positive values</strong>: Perfect positive correlation</li>
-        <li><strong>-large negative values</strong>: Perfect negative correlation</li>
+        <li><strong>Large positive values</strong>: Positive correlation</li>
+        <li><strong>-large negative values</strong>: Negative correlation</li>
         <li><strong>0</strong>: No correlation</li>
     </ul>
 

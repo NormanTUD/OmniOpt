77e5dac95fc3311ef8e5ff39c7335173a1ce32e0
n <norman.koch@tu-dresden.de>
2025-03-09 15:11:33 +0100

    fix

--- ax/.gui/_tutorials/help.php
+++ ax/.gui/_tutorials/help.php
@@ -1,5 +1,7 @@
 <div id="toc"></div>
 
+<!-- The <tt>--help</tt> of the main script of OmniOpt2 -->
+
 <h2 id="available_parameters_help">Available Parameters (--help)</h2>
 
 <p>An overview table of all arguments that OmniOpt2 accepts via the Command line. Same data as in <tt>./omniopt --help</tt>.</p>

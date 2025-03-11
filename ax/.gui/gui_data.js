f48c8b674547b51e736b73fa5a40d2930b1da6cd
Norman Koch <norman.koch@tu-dresden.de>
2025-03-11 15:27:57 +0100

    fix

--- ax/.gui/gui_data.js
+++ ax/.gui/gui_data.js
@@ -252,30 +252,6 @@ var hiddenTableData = [
 		`,
 		help: "The model chosen here tries to make an informed choice (except SOBOL, which means random search) about where to look for new hyperparameters. Different models are useful for different optimization problems, though which is best for what is something that I still need to search exactly (TODO!)"
 	},
-
-	{
-		label: "Optimization with combined criteria",
-		id: "occ_type",
-		type: "select",
-		value: "euclid",
-		options: [
-			{
-				text: "Calculate the euclidean distance to the origo of the search space",
-				value: "euclid"
-			},
-			{
-				text: "Calculate the geometric distance to the origo of the search space",
-				value: "geometric"
-			},
-			{
-				text: "Calculate the signed harmonic distance to the origo of the search space",
-				value: "signed_harmonic"
-			},
-		],
-		required: true,
-		info: "How to merge multiple results into one. Doesn't affect single result jobs.",
-		help: "How to merge multiple results into one."
-	},
 	{
 		label: "Installation-Method",
 		id: "installation_method",
@@ -338,7 +314,6 @@ var hiddenTableData = [
 		value: 0,
 		info: "Disable TQDM."
 	},
-
 	{
 		label: "Verbose TQDM?",
 		id: "verbose_tqdm",
@@ -401,6 +376,29 @@ var hiddenTableData = [
 		type: "checkbox",
 		value: 0,
 		info: "Use optimization with combined criteria (OCC)."
+	},
+	{
+		label: "Optimization with combined criteria type",
+		id: "occ_type",
+		type: "select",
+		value: "euclid",
+		options: [
+			{
+				text: "Calculate the euclidean distance to the origo of the search space",
+				value: "euclid"
+			},
+			{
+				text: "Calculate the geometric distance to the origo of the search space",
+				value: "geometric"
+			},
+			{
+				text: "Calculate the signed harmonic distance to the origo of the search space",
+				value: "signed_harmonic"
+			},
+		],
+		required: true,
+		info: "How to merge multiple results into one. Doesn't affect single result jobs.",
+		help: "How to merge multiple results into one."
 	},
 	{
 		label: "Stderr to Stdout?",

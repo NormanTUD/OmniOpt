bc97b227ceddcb9d6c833f11080a9667ff78f702
n <norman.koch@tu-dresden.de>
2025-03-13 20:59:23 +0100

    fix

--- .gui/darkmode.js
+++ .gui/darkmode.js
@@ -66,8 +66,6 @@ function enable_dark_mode() {
 	$(".window").css("background", "unset");
 
 	$(".mode-text").text("Light-Mode");
-
-	$('button[aria-selected="true"]').css("background-color", "red");
 }
 
 function enable_light_mode() {
@@ -93,8 +91,6 @@ function enable_light_mode() {
 	$(".window").css("background", "#ece9d8");
 
 	$(".mode-text").text("Dark-Mode");
-
-	$('button[aria-selected="true"]').css("background-color", "red");
 }
 
 function apply_theme_based_on_system_preferences() {

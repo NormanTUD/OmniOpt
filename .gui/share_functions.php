ff985b50eacb68e5e77311dff48fae8cdc0be44d
Norman Koch <norman.koch@tu-dresden.de>
2025-03-17 10:33:38 +0100

    Add descriptive button text for file downloads

--- .gui/share_functions.php
+++ .gui/share_functions.php
@@ -190,7 +190,7 @@
 		$filename = basename($filename);
 
 		$str = "<button onclick='copy_to_clipboard_from_id(\"".$id."\")'><span class='invert_in_dark_mode'>&#128203;</span> Copy raw data to clipboard</button>\n";
-		$str .= "<button onclick='download_as_file(\"".$id."\", \"".$filename."\")'>&DoubleDownArrow; Download as file</button>\n";
+		$str .= "<button onclick='download_as_file(\"".$id."\", \"".htmlentities($filename)."\")'>&DoubleDownArrow; Download &raquo;".htmlentities($filename)."&laquo; as file</button>\n";
 
 		return $str;
 	}

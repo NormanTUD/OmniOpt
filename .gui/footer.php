0c4bc90aecb9af845d53f7caa86c1ca65a10fbf0
n <norman.koch@tu-dresden.de>
2025-03-18 16:45:30 +0100

    remove footer

--- .gui/footer.php
+++ .gui/footer.php
@@ -1,3 +1,4 @@
+		<!--
 		<footer id="footer_element">
 			<p style="margin: 0px">
 				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2/<a href="https://scads.ai/" target="_blank">ScaDS</a> |
@@ -6,6 +7,7 @@
 				<a href="mailto:norman.koch@scads.ai">Contact Us</a>
 			</p>
 		</footer>
+		-->
 		<script>
 			apply_theme_based_on_system_preferences();
 		</script>

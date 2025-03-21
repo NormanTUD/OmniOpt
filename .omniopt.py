02c62b18fcacdf9639bf90a49f892f21396ee016
n <norman.koch@tu-dresden.de>
2025-03-21 22:09:42 +0100

    fix

--- .omniopt.py
+++ .omniopt.py
@@ -5839,7 +5839,7 @@ def get_generation_strategy() -> GenerationStrategy:
 
         try:
             with open(model_file, mode="w", encoding="utf-8") as f:
-                f.write(chosen_model)
+                f.write(str(chosen_model))
         except Exception as e:
             print_red(f"Failed writing '{model_file}': {e}")
 

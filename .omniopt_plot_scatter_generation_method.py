fd67866918461159ae0f8f48378eddc8b3a714cf
n <norman.koch@tu-dresden.de>
2025-03-23 12:10:29 +0100

    removed duplicate code

--- .omniopt_plot_scatter_generation_method.py
+++ .omniopt_plot_scatter_generation_method.py
@@ -59,16 +59,10 @@ def plot_graph(dataframe: pd.DataFrame, save_to_file: Union[None, str] = None) -
                 plt.show()
 
 @beartype
-def handle_empty_data(filepath: str) -> None:
+def handle_error(filepath: str, errmsg: str, exit_code: int) -> None:
     if not os.environ.get("NO_NO_RESULT_ERROR"):
-        print(f"Could not find values in file {filepath}")
-    sys.exit(19)
-
-@beartype
-def handle_unicode_error(filepath: str) -> None:
-    if not os.environ.get("PLOT_TESTS"):
-        print(f"{filepath} seems to be invalid utf8.")
-    sys.exit(7)
+        print(errmsg)
+    sys.exit(exit_code)
 
 @beartype
 def update_graph() -> None:
@@ -96,9 +90,9 @@ def update_graph() -> None:
     except FileNotFoundError:
         print(f"File not found: {csv_path}")
     except pd.errors.EmptyDataError:
-        handle_empty_data(csv_path)
+        handle_error(filepath, f"Could not find values in file {csv_path}", 19)
     except UnicodeDecodeError:
-        handle_unicode_error(csv_path)
+        handle_error(filepath, f"{csv_path} seems to be invalid utf8.", 7)
     except KeyError:
         if not os.environ.get("PLOT_TESTS"):
             print(f"{csv_path} seems to have no 'result' column.")

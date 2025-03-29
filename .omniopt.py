84c18a0c2b1dd368ea503437de5f4dadadcfc41c
norman koch alpha <norman.koch@tu-dresden.de>
2025-03-29 17:46:25 +0100

    unsafe when marking as running

--- .omniopt.py
+++ .omniopt.py
@@ -4902,7 +4902,7 @@ def finish_job_core(job: Any, trial_index: int, this_jobs_finished: int) -> int:
             if job:
                 try:
                     progressbar_description(["job_failed"])
-                    _trial.mark_running(no_runner_required=True)
+                    _trial.mark_running(no_runner_required=True, unsafe=True)
                     ax_client.log_trial_failure(trial_index=trial_index)
                 except Exception as e:
                     print(f"ERROR while trying to mark job as failure in line {get_line_info()}: {e}")

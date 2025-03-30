026eaccf3a0b4ed9500168e6a9b8d68f39e9b869
n <norman.koch@tu-dresden.de>
2025-03-30 18:11:08 +0200

    terminal svg

--- .gui/share_functions.php
+++ .gui/share_functions.php
@@ -403,7 +403,7 @@
 
 			$html .= copy_id_to_clipboard_string("simple_pre_tab_$id", $filename);
 
-			$svg_icon = get_icon_html("text.svg");
+			$svg_icon = get_icon_html("terminal.svg");
 
 			$tabs["$svg_icon$name"] = [
 				'id' => $id,

fa6031b3d69b70a0efa48e969f19035c0c4e243d
Norman Koch <norman.koch@tu-dresden.de>
2025-03-30 14:28:00 +0200

    added clipboard

--- .gui/share_functions.php
+++ .gui/share_functions.php
@@ -188,7 +188,7 @@
 	function copy_id_to_clipboard_string($id, $filename) {
 		$filename = basename($filename);
 
-		$str = "<button onclick='copy_to_clipboard_from_id(\"".$id."\")'><span class='invert_in_dark_mode'>&#128203;</span> Copy raw data to clipboard</button>\n";
+		$str = "<button onclick='copy_to_clipboard_from_id(\"".$id."\")'><img src='clipboard.svg' style='height: 1em'> Copy raw data to clipboard</button>\n";
 		$str .= "<button onclick='download_as_file(\"".$id."\", \"".htmlentities($filename)."\")'>&DoubleDownArrow; Download &raquo;".htmlentities($filename)."&laquo; as file</button>\n";
 
 		return $str;

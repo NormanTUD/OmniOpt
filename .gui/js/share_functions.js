88c38a61702b48ae9a8e71ee6543259bced83314
Norman Koch <norman.koch@tu-dresden.de>
2025-04-01 19:40:16 +0200

    different colors

--- .gui/js/share_functions.js
+++ .gui/js/share_functions.js
@@ -1702,7 +1702,7 @@ function plotResultsDistributionByGenerationMethod() {
 }
 
 function plotJobStatusDistribution() {
-	if ("true" === $("#plotJobStatusDistribution").data("loaded")) {
+	if ($("#plotJobStatusDistribution").data("loaded") === "true") {
 		return;
 	}
 
@@ -1711,13 +1711,17 @@ function plotJobStatusDistribution() {
 
 	tab_results_csv_json.forEach(row => {
 		var status = row[tab_results_headers_json.indexOf(status_col)];
-		status_counts[status] = (status_counts[status] || 0) + 1;
+		if (status) {
+			status_counts[status] = (status_counts[status] || 0) + 1;
+		}
 	});
 
 	var statuses = Object.keys(status_counts);
 	var counts = Object.values(status_counts);
 
-	var colors = statuses.map((status, i) => status === "FAILED" ? "red" : `hsl(${(i * 137) % 360}, 70%, 50%)`);
+	var colors = statuses.map((status, i) =>
+		status === "FAILED" ? "#FF0000" : `hsl(${30 + ((i * 137) % 330)}, 70%, 50%)`
+	);
 
 	var trace = {
 		x: statuses,

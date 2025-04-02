541191613fd0bb2959bae63ccc49a4199315a7e3
Norman Koch <norman.koch@tu-dresden.de>
2025-04-02 08:39:33 +0200

    Update BO_MIXED description in gui_data.js

--- .gui/gui_data.js
+++ .gui/gui_data.js
@@ -253,7 +253,7 @@ var hiddenTableData = [
 			    <li>FACTORIAL: <a target='_blank' href='https://ax.dev/docs/tutorials/factorial/'>All possible combinations</a></li>
 			    <li>SAASBO: <i><a target='_blank' href='https://arxiv.org/pdf/2103.00349'>Sparse Axis-Aligned Subspace Bayesian Optimization</a></i> for high-dimensional Bayesian Optimization, recommended for hundreds of dimensions</li>
 			    <li>UNIFORM: Random (uniformly distributed)</li>
-			    <li>BO_MIXED: '<i><a href='https://ax.dev/api/_modules/ax/modelbridge/dispatch_utils.html'>BO_MIXED</a></i>' optimizes all range parameters once for each combination of choice parameters, then takes the optimum of those optima. The cost associated with this method grows with the number of combinations, and so it is only used when the number of enumerated discrete combinations is below some maximum value.</li>
+			    <li>BO_MIXED: '<i>BO_MIXED</i>' optimizes all range parameters once for each combination of choice parameters, then takes the optimum of those optima. The cost associated with this method grows with the number of combinations, and so it is only used when the number of enumerated discrete combinations is below some maximum value.</li>
 			</ul>
 		`,
 		help: "The model chosen here tries to make an informed choice (except SOBOL, which means random search) about where to look for new hyperparameters. Different models are useful for different optimization problems, though which is best for what is something that I still need to search exactly (TODO!)"

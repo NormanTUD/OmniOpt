f07b2fc24ed823353b0057fc73757ea5555883e1
n <norman.koch@tu-dresden.de>
2025-04-11 20:43:28 +0200

    better?

--- .tools/example_external.py
+++ .tools/example_external.py
@@ -1,31 +1,47 @@
+from pprint import pprint
 import sys
 import os
 import json
 import random
 
-def generate_random_value(parameter):
-    if parameter['parameter_type'] == 'RANGE':
-        range_min, range_max = parameter['range']
-        if parameter['type'] == 'INT':
-            return random.randint(range_min, range_max)
-        elif parameter['type'] == 'FLOAT':
-            return random.uniform(range_min, range_max)
-    elif parameter['parameter_type'] == 'CHOICE':
-        values = parameter['values']
-        if parameter['type'] == 'INT':
-            return random.choice(values)
-        elif parameter['type'] == 'STRING':
-            return random.choice(values)
-        else:
-            return random.choice(values)  # For FLOAT or other types, choose one value
+def dier(msg):
+    pprint(msg)
+    sys.exit(10)
 
-    elif parameter['parameter_type'] == 'FIXED':
-        return parameter['value']
+def generate_random_value(parameter):
+    try:
+        if parameter['parameter_type'] == 'RANGE':
+            range_min, range_max = parameter['range']
+            if parameter['type'] == 'INT':
+                return random.randint(range_min, range_max)
+            elif parameter['type'] == 'FLOAT':
+                return random.uniform(range_min, range_max)
+        elif parameter['parameter_type'] == 'CHOICE':
+            values = parameter['values']
+            if parameter['type'] == 'INT':
+                return random.choice(values)
+            elif parameter['type'] == 'STRING':
+                return random.choice(values)
+            else:
+                return random.choice(values)  # Für FLOAT oder andere Typen
+        elif parameter['parameter_type'] == 'FIXED':
+            return parameter['value']
+    except KeyError as e:
+        print(f"KeyError: Missing {e} in parameter")
+        sys.exit(4)  # Beende das Skript mit einem Fehlercode
 
 def generate_random_point(parameters):
     point = {}
-    for param_name, param_data in parameters.items():
-        point[param_name] = generate_random_value(param_data)
+    for param_data in parameters.items():
+        p = param_data[1]
+
+        if(p):
+            if not isinstance(p, list):
+                p_keys = list(p.keys())
+
+                for param_name in p_keys:
+                    this_param = param_data[1][param_name]
+                    point[param_name] = generate_random_value(this_param)
     return point
 
 def main():

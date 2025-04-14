bb167abacc1f656afaba5466f4c3d22b9553a3df
n <norman.koch@tu-dresden.de>
2025-04-14 18:55:19 +0200

    allow 0 jobs for sobol models and EXTERNAL_GENERATOR

--- .omniopt.py
+++ .omniopt.py
@@ -6947,7 +6947,7 @@ def set_orchestrator() -> None:
 @beartype
 def check_if_has_random_steps() -> None:
     with console.status("[bold green]Checking if has random steps..."):
-        if (not args.continue_previous_job and "--continue" not in sys.argv) and (args.num_random_steps == 0 or not args.num_random_steps):
+        if (not args.continue_previous_job and "--continue" not in sys.argv) and (args.num_random_steps == 0 or not args.num_random_steps) and args.model not in ["EXTERNAL_GENERATOR", "SOBOL"]:
             print_red("You have no random steps set. This is only allowed in continued jobs. To start, you need either some random steps, or a continued run.")
             my_exit(233)
 

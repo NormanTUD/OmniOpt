342f4f452ca0f1f99b68f8a587ca4483513936db
n <norman.koch@tu-dresden.de>
2025-04-16 19:44:51 +0200

    readded progress bar log

--- .gui/share.php
+++ .gui/share.php
@@ -96,7 +96,7 @@
 				["$run_dir/oo_errors.txt", "Errors", "tab_errors", true],
 				["$run_dir/outfile", "Main-Log", "tab_main_log", true],
 				//["$run_dir/trial_index_to_params", "Trial-Index-to-Param", "tab_trial_index_to_param"],
-				//["$run_dir/progressbar", "Progressbar log", "tab_progressbar_log"],
+				["$run_dir/progressbar", "Progressbar log", "tab_progressbar_log"],
 				["$run_dir/verbose_log.txt", "Verbose log", "tab_verbose_log"],
 			],
 			'add_simple_table_from_ascii_table_file' => [

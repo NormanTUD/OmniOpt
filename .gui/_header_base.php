8443ffce8aa37ae686245bf138247fa40211f4f1
Norman Koch <norman.koch@tu-dresden.de>
2025-05-05 07:42:22 +0200

    moved stuff around, new font

--- .gui/_header_base.php
+++ .gui/_header_base.php
@@ -73,6 +73,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>OmniOpt2</title>
 		<link href="prism.css" rel="stylesheet">
+		<link href="css/source_sans_pro.css" rel="stylesheet">
+		<link href="css/ibm_plex_font.css" rel="stylesheet">
 		<link rel="icon" type="image/x-icon" href="favicon.ico">
 		<?php js("jquery-3.7.1.js"); ?>
 		<?php js("jquery-ui.min.js"); ?>

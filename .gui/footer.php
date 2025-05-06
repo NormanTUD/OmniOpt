6ae5786c08c3703943976b37f17127f7be33bf22
Norman Koch <norman.koch@tu-dresden.de>
2025-05-06 08:16:17 +0200

    using xp.css's way of creating a footer

--- .gui/footer.php
+++ .gui/footer.php
@@ -1,12 +1,20 @@
 		<div style="height: 100px;"></div>
-		<footer id="footer_element">
-			<p style="margin: 0px">
-				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2/<a href="https://scads.ai/" target="_blank">ScaDS</a> |
-				<a href="https://scads.ai/imprint/" target="_blank">Imprint/Impressum</a> |
-				<a href="https://scads.ai/privacy/" target="_blank">Privacy Policy</a> |
+		<div class="status-bar" id="footer_element">
+			<p class="status-bar-field">
+				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2 /
+				<a href="https://scads.ai/" target="_blank">ScaDS</a>
+			</p>
+			<p class="status-bar-field">
+				<a href="https://scads.ai/imprint/" target="_blank">Imprint/Impressum</a>
+			</p>
+			<p class="status-bar-field">
+				<a href="https://scads.ai/privacy/" target="_blank">Privacy Policy</a>
+			</p>
+			<p class="status-bar-field">
 				<a href="mailto:norman.koch@scads.ai">Contact Us</a>
 			</p>
-		</footer>
+		</div>
+
 		<script>
 			apply_theme_based_on_system_preferences();
 		</script>

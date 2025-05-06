d779c0cea6a32021cd67d6aa08afd276028db65d
Norman Koch <norman.koch@tu-dresden.de>
2025-05-06 07:18:56 +0200

    re-enable footer

--- .gui/footer.php
+++ .gui/footer.php
@@ -1,4 +1,4 @@
-		<!--
+		<div style="height: 100px;"></div>
 		<footer id="footer_element">
 			<p style="margin: 0px">
 				&copy; 2024-<?php echo date('Y'); ?> OmniOpt2/<a href="https://scads.ai/" target="_blank">ScaDS</a> |
@@ -7,7 +7,6 @@
 				<a href="mailto:norman.koch@scads.ai">Contact Us</a>
 			</p>
 		</footer>
-		-->
 		<script>
 			apply_theme_based_on_system_preferences();
 		</script>

5bae9e16708ba563845482ee9d05e18b3f9c10b3
n <norman.koch@tu-dresden.de>
2025-05-09 14:24:25 +0200

    ignore exception, just load the newer version when needed

--- .omniopt.py
+++ .omniopt.py
@@ -766,7 +766,7 @@ try:
         import ax.modelbridge.generation_node
         try:
             from ax.modelbridge.model_spec import ModelSpec
-        except Exception as e:
+        except Exception:
             from ax.generation_strategy.model_spec import ModelSpec
         from ax.modelbridge.generation_strategy import (GenerationStep, GenerationStrategy)
         from ax.modelbridge.registry import Models

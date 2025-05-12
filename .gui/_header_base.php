cbfb81ea02d0909ae34f072a0de96b5cbea00c64
n <norman.koch@tu-dresden.de>
2025-05-12 06:26:06 +0200

    added cm, even though its not used

--- .gui/_header_base.php
+++ .gui/_header_base.php
@@ -74,7 +74,6 @@
 		<title>OmniOpt2</title>
 		<link href="prism.css" rel="stylesheet">
 		<link href="css/source_sans_pro.css" rel="stylesheet">
-		<link href="css/neo_euler.css" rel="stylesheet">
 		<link rel="icon" type="image/x-icon" href="favicon.ico">
 		<?php js("jquery-3.7.1.js"); ?>
 		<?php js("jquery-ui.min.js"); ?>

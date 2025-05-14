bf8b4afbac92ad20860166533830f65a3ab00039
Norman Koch <norman.koch@tu-dresden.de>
2025-05-14 07:25:38 +0200

    use var www again

--- .gui/docker.sh
+++ .gui/docker.sh
@@ -123,12 +123,12 @@ docker_compose up -d || {
 	exit 255
 }
 
-docker_compose exec php-web chown -R www-data:www-data $SHARES_PATH || {
+docker_compose exec php-web chown -R www-data:www-data /var/www/html/shares || {
 	echo "Failed to set ownership inside container"
 	exit 256
 }
 
-docker_compose exec php-web chmod 755 $SHARES_PATH || {
+docker_compose exec php-web chmod 755 /var/www/html/shares || {
 	echo "Failed to set ownership inside container"
 	exit 256
 }

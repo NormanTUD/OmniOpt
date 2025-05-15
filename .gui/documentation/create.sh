7ce5bbb168bba6862a2076f8d0355ba7fd4eb938
n <norman.koch@tu-dresden.de>
2025-05-15 16:36:04 +0200

    show msg if fail

--- .gui/documentation/create.sh
+++ .gui/documentation/create.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+if ! command -v plantuml >/dev/null 2>/dev/null; then
+	echo "plantuml is not installed. Try to install it, e.g. via"
+	echo "sudo apt-install plantuml"
+	echo "Cannot continue."
+	exit 1
+fi
+
 plantuml -DMODE_DARK=true -tsvg architecture.puml -o output_dark
 plantuml -DMODE_DARK=false -tsvg architecture.puml -o output_light
+
+exit 0

9f56c917fe3dc3d056333a4824763d113f519d36
n <norman.koch@tu-dresden.de>
2025-05-25 20:36:20 +0200

    int float

--- .omniopt.py
+++ .omniopt.py
@@ -3341,7 +3341,7 @@ def evaluate(parameters: dict) -> Optional[Union[int, float, Dict[str, Optional[
 
     if _test_gpu is None:
         parameters = {
-            k: (float(v) if isinstance(v, (int, float, str)) and re.fullmatch(r'^\d+(\.0+)?$', str(v)) else v)
+            k: (int(float(v)) if isinstance(v, (int, float, str)) and re.fullmatch(r'^\d+(\.0+)?$', str(v)) else v)
             for k, v in parameters.items()
         }
 

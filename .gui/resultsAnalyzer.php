05da3fe99eeca25d33eb36926e970ede95a16b9a
Norman Koch <norman.koch@tu-dresden.de>
2025-07-11 21:21:13 +0200

    fix

--- .gui/resultsAnalyzer.php
+++ .gui/resultsAnalyzer.php
@@ -497,7 +497,7 @@ function computeDirectionalInfluenceFlat(string $csvPath, array $correlations, a
 			$html .= "<td>{$info['certainty']}</td>";
 			$html .= "<td>{$info['r']}</td>";
 
-			$width = 800;
+			$width = 1000;
 			$height = count($paramInfos);
 			$im = imagecreatetruecolor($width, $height);
 

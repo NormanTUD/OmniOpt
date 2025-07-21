fdf826a029341a44cec3cc1005d9d8abb7a64e2f
Norman Koch <norman.koch@tu-dresden.de>
2025-07-21 07:14:01 +0200

    allow dynamic creation of install_omniax.sh

--- .gui/404.php
+++ .gui/404.php
@@ -1,4 +1,20 @@
 <?php
+	include_once("_functions.php");
+
+	$prefix = isset($_SERVER["CONTEXT_PREFIX"]) ? $_SERVER["CONTEXT_PREFIX"] : "";
+	$requested_file = $_SERVER["REQUEST_URI"];
+
+	$escaped_prefix = preg_quote($prefix, '/');
+	$pattern = '/^' . $escaped_prefix . '/';
+
+	$filename = preg_replace($pattern, '', $requested_file);
+
+	if($filename == "install_omniax.sh" || $filename == "install_omniopt.sh" || $filename == "install_omniopt2.sh") {
+		//header('Content-Type: application/bash');
+		include("install_omniax.php");
+		exit(0);
+	}
+
 	http_response_code(404);
 
 	include("_header_base.php");

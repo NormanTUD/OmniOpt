e4119a8f821889ade5dc2f0a246ac1116256482b
n <norman.koch@tu-dresden.de>
2025-07-26 16:42:11 +0200

    removed unused value

--- .omniopt.py
+++ .omniopt.py
@@ -794,7 +794,7 @@ class ConfigLoader:
 
         validated_config = self.validate_and_convert(config, arg_defaults)
 
-        for key, value in vars(cli_args).items():
+        for key, _ in vars(cli_args).items():
             if key in validated_config:
                 setattr(cli_args, key, validated_config[key])
 

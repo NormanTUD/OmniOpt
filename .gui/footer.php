d84245022ccdeb7e74fd00d941045e29c5d09966
n <norman.koch@tu-dresden.de>
2025-08-27 10:23:51 +0200

    removed date

--- .gui/footer.php
+++ .gui/footer.php
@@ -3,9 +3,6 @@
 			<p class="status-bar-field">
 				<a href="https://scads.ai/privacy/" target="_blank">Privacy Policy</a>
 			</p>
-			<p class="status-bar-field hide_on_mobile">
-				&copy; 2024-<?php echo gmdate('Y'); ?> <a href="https://scads.ai/" target="_blank">ScaDS.ai</a>
-			</p>
 			<p class="status-bar-field">
 				<a href="mailto:norman.koch@scads.ai">Contact Us</a>
 			</p>

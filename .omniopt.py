acbcbe407edd223b73e2cc4278246a6cd1bb6cb6
n <norman.koch@tu-dresden.de>
2025-08-28 07:40:33 +0200

    less mypy

--- .omniopt.py
+++ .omniopt.py
@@ -449,7 +449,7 @@ RESET: str = "\033[0m"
 
 uuid_regex: Pattern = re.compile(r"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89aAbB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
 
-worker_generator_uuid: str = uuid.uuid4()
+worker_generator_uuid: str = str(uuid.uuid4())
 
 new_uuid: str = str(uuid.uuid4())
 run_uuid: str = os.getenv("RUN_UUID", new_uuid)

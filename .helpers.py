aba42e2f9dc1a0c5c2b579719aba3e502acd8b47
Norman Koch <norman.koch@tu-dresden.de>
2025-09-01 10:07:08 +0200

    removed python version check

--- .helpers.py
+++ .helpers.py
@@ -227,12 +227,6 @@ def print_color(color: str, text: str) -> None:
         print(f"Error: {e}")
         print(text)
 
-def check_python_version() -> None:
-    python_version = platform.python_version()
-    supported_versions = ["3.10.4", "3.10.12", "3.11.2", "3.11.9", "3.9.2", "3.11.3", "3.12.3", "3.12.4", "3.12.5", "3.12.6", "3.12.7", "3.12.7+", "3.12.8", "3.13.1", "3.13.2", "3.13.3", "3.13.5", "3.13.7"]
-    if python_version not in supported_versions:
-        print_color("yellow", f"Warning: Supported python versions are {', '.join(supported_versions)}, but you are running {python_version}. This may or may not cause problems. Just is just a warning.")
-
 def create_widgets(_data: Any) -> Any:
     _plt, button, MAXIMUM_TEXTBOX, MINIMUM_TEXTBOX, _args, TEXTBOX_MINIMUM, TEXTBOX_MAXIMUM, update_graph = _data
 
@@ -882,5 +876,3 @@ def die_if_cannot_be_plotted(run_dir: Optional[str]) -> None:
     if not can_be_plotted(run_dir):
         log_error(f"{run_dir} contains multiple RESULTS and thus can only be plotted by parallel plot")
         sys.exit(2)
-
-check_python_version()
